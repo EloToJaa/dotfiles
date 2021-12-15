018d372a5b87441eabafb60fd66cbfd451942651
Łukasz Budziak <lukaszbudziak122@gmail.com>
2021-12-15 14:54:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,42 +11,48 @@ result () {
 }
 
 echo "Updating packages"
-apt update -y
+sudo apt update -y
 
 read -p "Do you want to upgrade (Y/N)? " ans
 result "$ans"
 if [ $? -eq 1 ]; then
-    apt full-upgrade -y
+    sudo apt full-upgrade -y
 fi
 
 read -p "Do you want to install git and curl (Y/N)? " ans
 result "$ans"
 if [ $? -eq 1 ]; then
-    apt install -y curl git
+    sudo apt install -y curl git
 fi
 
 read -p "Do you want to install C compilers (Y/N)? " ans
 result "$ans"
 if [ $? -eq 1 ]; then
-    apt install -y gcc g++ make
+    sudo apt install -y gcc g++ make
 fi
 
 read -p "Do you want to install Python (Y/N)? " ans
 result "$ans"
 if [ $? -eq 1 ]; then
-    apt install -y python3 python3-dev python3-pip python3-setuptools
+    sudo apt install -y python3 python3-dev python3-pip python3-setuptools
+fi
+
+read -p "Do you want to install Node (Y/N)? " ans
+result "$ans"
+if [ $? -eq 1 ]; then
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 fi
 
 read -p "Do you want to install Vim (Y/N)? " ans
 result "$ans"
 if [ $? -eq 1 ]; then
-    apt install -y neovim
+    sudo apt install -y neovim
 fi
 
 read -p "Do you want to install Fish (Y/N)? " ans
 result "$ans"
 if [ $? -eq 1 ]; then
-    apt install -y fish thefuck grc
+    sudo apt install -y fish thefuck grc
     curl -L https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | bash
     omf install budspencer nvm grc bang-bang fuck expand
     set budspencer_colors 000000 333333 666666 ffffff 2ea4c9 ff0000 ff0000 ff66ff 3300ff ffffff 00ffff 00ff00
