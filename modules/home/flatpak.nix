ad2e6bb5b3b08630cc5f120f45e30de887bf94ed
EloToJaa <elotoja@protonmail.com>
2024-10-04 19:08:22 +0200

    Rename import

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -1,6 +1,6 @@
 { inputs, ... }: 
 {
-  imports = [ inputs.nix-flatpak.homeManagerModules.default ];
+  imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
   services.flatpak = {
     packages = [
       { appId = "io.github.zen_browser.zen"; origin = "flathub"; }
