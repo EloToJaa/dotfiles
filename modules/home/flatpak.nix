479549fc0430b0e41dcd5b00ef10e889c56321c0
EloToJaa <elotoja@protonmail.com>
2024-10-04 19:07:33 +0200

    Enable flatpak

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -2,7 +2,6 @@
 {
   imports = [ inputs.nix-flatpak.homeManagerModules.default ];
   services.flatpak = {
-    enable = true;
     packages = [
       { appId = "io.github.zen_browser.zen"; origin = "flathub"; }
     ];
