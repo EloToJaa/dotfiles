d948813c8a4bc921e27a3a2ee4d48cab171c4569
EloToJaa <elotoja@protonmail.com>
2024-10-04 18:22:12 +0200

    Fix syntax

--- modules/home/brave/default.nix
+++ modules/home/brave/default.nix
@@ -2,6 +2,6 @@
 {
   # home.packages = (with pkgs; [ brave ]);
   services.flatpak.packages = [
-    { appId = "com.brave.Browser"; origin = "flathub" }
+    { appId = "com.brave.Browser"; origin = "flathub"; }
   ];
 }
