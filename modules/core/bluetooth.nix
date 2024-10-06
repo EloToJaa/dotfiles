26bbb7a3ef6909b49a9b64f01fd557212cd1fb9f
EloToJaa <elotoja@protonmail.com>
2024-10-06 21:05:43 +0200

    Add bluetooth pulseaudio support

--- modules/core/bluetooth.nix
+++ modules/core/bluetooth.nix
@@ -11,4 +11,14 @@
     wantedBy = [ "default.target" ];
     serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
   };
+  
+  hardware.pulseaudio.configFile = pkgs.writeText "default.pa" ''
+    load-module module-bluetooth-policy
+    load-module module-bluetooth-discover
+    ## module fails to load with 
+    ##   module-bluez5-device.c: Failed to get device path from module arguments
+    ##   module.c: Failed to load module "module-bluez5-device" (argument: ""): initialization failed.
+    # load-module module-bluez5-device
+    # load-module module-bluez5-discover
+  '';
 }
