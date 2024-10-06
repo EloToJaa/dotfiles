76b6d2bf70e4c13c6df35205c646e4155d6b9fba
EloToJaa <elotoja@protonmail.com>
2024-10-06 18:24:38 +0200

    Move flatpak back

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -2,7 +2,6 @@
 {
   imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
   services.flatpak = {
-    enable = true;
     packages = [
       { appId = "io.github.zen_browser.zen"; origin = "flathub"; }
     ];
