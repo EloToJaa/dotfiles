29f3c1c4afcc247ad0d40ce0ad8f60e1727caea1
EloToJaa <elotoja@protonmail.com>
2024-10-06 18:22:23 +0200

    Fix spelling mistake

--- modules/home/flatpak.nix
+++ modules/home/flatpak.nix
@@ -2,7 +2,7 @@
 {
   imports = [ inputs.nix-flatpak.homeManagerModules.nix-flatpak ];
   services.flatpak = {
-    enabled = true;
+    enable = true;
     packages = [
       { appId = "io.github.zen_browser.zen"; origin = "flathub"; }
     ];
