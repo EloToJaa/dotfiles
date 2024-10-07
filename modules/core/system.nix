876b8d6ae71bbfdca10953ea100ea1dd10a4bb82
EloToJaa <elotoja@protonmail.com>
2024-10-07 18:25:44 +0200

    Change UK to GB locale

--- modules/core/system.nix
+++ modules/core/system.nix
@@ -21,7 +21,7 @@
   ];
 
   time.timeZone = "Europe/Warsaw";
-  i18n.defaultLocale = "en_UK.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
   nixpkgs.config.allowUnfree = true;
   system.stateVersion = "24.05";
 }
