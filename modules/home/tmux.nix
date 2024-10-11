04dd201d1a8e77b0b30480f8ea007716c5e1b48c
EloToJaa <elotoja@protonmail.com>
2024-10-11 22:49:54 +0200

    Remove git fetch of super fingers

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -1,17 +1,4 @@
 { pkgs, variables, ... }:
-let
-  tmux-super-fingers = pkgs.tmuxPlugins.mkTmuxPlugin
-    {
-      pluginName = "tmux-super-fingers";
-      version = "unstable-2024-10-04";
-      src = pkgs.fetchFromGitHub {
-        owner = "artemave";
-        repo = "tmux_super_fingers";
-        rev = "2771f791a03880b3653c043cff48ee81db66212b";
-        hash = "sha256-GnVlV8JRKVx6muVKYvqkCSMds7IBTYp1NFEgQnnuYEc=";
-      };
-    };
-in
 {
   programs.tmux = {
     enable = true;
