867308e28bdf43670274d0c0c01ee5d4724fc5ec
EloToJaa <elotoja@protonmail.com>
2024-10-11 14:50:57 +0200

    Add mullvad VPN

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -11,6 +11,13 @@
     };
   };
 
+  services.mullvad-vpn.enable = true;
+  services.mullvad-vpn.package = pkgs.mullvad-vpn;
+  /* Required to get DNS working with mullvad.
+   * https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/15
+   */
+  networking.resolvconf.enable = false;
+
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
   ];
