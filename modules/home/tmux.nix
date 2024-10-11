193785b4ffc82b4cdae1535e9fb7cf7f28f03b77
EloToJaa <elotoja@protonmail.com>
2024-10-11 22:37:15 +0200

    Remove icon from window status tmux

--- modules/home/tmux.nix
+++ modules/home/tmux.nix
@@ -33,7 +33,6 @@ in
         set -g @catppuccin_window_status_style "rounded"
         set -g @catppuccin_window_default_text "#W"
         set -g @catppuccin_window_current_text "#W"
-        set -g @catppuccin_window_status "icon"
         '';
       }
       tmuxPlugins.vim-tmux-navigator
@@ -56,21 +55,26 @@ in
       }
     ];
     extraConfig = ''
+    #base options
     set-option -sa terminal-overrides ",xterm*:Tc"
     set -g mouse on
 
+    # base index
     set -g base-index 1
     set -g pane-base-index 1
     set-window-option -g pane-base-index 1
     set-option -g renumber-windows on
 
+    # prefix
     unbind C-b
     set-option -g prefix C-a
     bind-key C-a send-prefix
 
+    # window splitting
     bind v split-window -v -c "#{pane_current_path}"
     bind h split-window -h -c "#{pane_current_path}"
 
+    # yank
     bind-key -T copy-mode-vi v send-keys -X begin-selection
     bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
     bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
@@ -82,6 +86,9 @@ in
     # rename
     bind-key r command-prompt "rename-window '%%'"
     bind-key R command-prompt "rename-session '%%'"
+
+    # reload config
+    bind-key f source-file ~/.config/tmux/tmux.conf
     '';
   };
 }
