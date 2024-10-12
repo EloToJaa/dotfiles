7f6ff741dca8de4ec7efc6975e890dca36577a4f
EloToJaa <elotoja@protonmail.com>
2024-10-13 00:50:55 +0200

    Fix config

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -19,36 +19,36 @@
       };
     };
 
-    plugins.nvim-cmp = {
+    plugins.cmp = {
       enable = true;
       autoEnableSources = true;
-      sources = [
+      settings.sources = [
         { name = "nvim_lsp"; }
         { name = "path"; }
         { name = "buffer"; }
       ];
 
-      mapping = {
-        "<CR>" = "cmp.mapping.confirm({ select = true })";
-        "<Tab>" = {
-          action = ''
-          function(fallback)
-            if cmp.visible() then
-              cmp.select_next_item()
-            elseif luasnip.expandable() then
-              luasnip.expand()
-            elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-            elseif check_backspace() then
-              fallback()
-            else
-              fallback()
-            end
-          end
-          '';
-          modes = [ "i" "s" ];
-        };
-      };
+      # mapping = {
+      #   "<CR>" = "cmp.mapping.confirm({ select = true })";
+      #   "<Tab>" = {
+      #     action = ''
+      #     function(fallback)
+      #       if cmp.visible() then
+      #         cmp.select_next_item()
+      #       elseif luasnip.expandable() then
+      #         luasnip.expand()
+      #       elseif luasnip.expand_or_jumpable() then
+      #         luasnip.expand_or_jump()
+      #       elseif check_backspace() then
+      #         fallback()
+      #       else
+      #         fallback()
+      #       end
+      #     end
+      #     '';
+      #     modes = [ "i" "s" ];
+      #   };
+      # };
     };
   };
 }
