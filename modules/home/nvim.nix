731f1185320e80b53a4d2e2d670041eb60df419a
EloToJaa <elotoja@protonmail.com>
2024-10-13 00:45:35 +0200

    Move mapping in nvim

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -27,27 +27,27 @@
         { name = "path"; }
         { name = "buffer"; }
       ];
-    };
 
-    mapping = {
-      "<CR>" = "cmp.mapping.confirm({ select = true })";
-      "<Tab>" = {
-        action = ''
-        function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
+      mapping = {
+        "<CR>" = "cmp.mapping.confirm({ select = true })";
+        "<Tab>" = {
+          action = ''
+          function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expandable() then
+              luasnip.expand()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            elseif check_backspace() then
+              fallback()
+            else
+              fallback()
+            end
           end
-        end
-        '';
-        modes = [ "i" "s" ];
+          '';
+          modes = [ "i" "s" ];
+        };
       };
     };
   };
