1231dcca6f06422f3a484b5aec9959f4dd59f224
EloToJaa <elotoja@protonmail.com>
2024-10-15 22:44:57 +0200

    Test cliphist

--- modules/home/hyprland/hyprland.nix
+++ modules/home/hyprland/hyprland.nix
@@ -9,12 +9,13 @@
     grim
     slurp
     wl-clip-persist
-    cliphist
+    # cliphist
     wf-recorder
     glib
     wayland
     direnv
   ];
+  
   systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
   wayland.windowManager.hyprland = {
     enable = true;
