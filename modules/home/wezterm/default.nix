f9b1cf6110765f560c1898fce88c119802d1a751
EloToJaa <elotoja@protonmail.com>
2024-10-19 22:12:05 +0200

    Update wezterm flake

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -1,26 +1,6 @@
-{ inputs, pkgs, host, config, ... }:
+{ inputs, pkgs, ... }:
 {
   home.packages = [ inputs.wezterm.packages.${pkgs.system}.default ];
-  # programs.wezterm = {
-  #   enable = true;
-  #   package = inputs.wezterm.packages.${pkgs.system}.default;
-  #   extraConfig = ''
-  #     local Config = require('config')
-
-  #     require('events.right-status').setup()
-  #     require('events.left-status').setup()
-  #     require('events.tab-title').setup()
-  #     require('events.new-tab-button').setup()
-
-  #     return Config:init()
-  #       :append(require('config.appearance'))
-  #       :append(require('config.bindings'))
-  #       :append(require('config.domains'))
-  #       :append(require('config.fonts'))
-  #       :append(require('config.general'))
-  #       :append(require('config.launch')).options
-  #   '';
-  # };
 
   xdg.configFile."wezterm".source = ./wezterm;
 }
