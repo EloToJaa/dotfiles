c742a57df8dc4beed98fa8acd387bc56bf254b34
EloToJaa <elotoja@protonmail.com>
2024-10-19 11:02:49 +0200

    Fix cells utils module

--- modules/home/wezterm/utils/cells.lua
+++ modules/home/wezterm/utils/cells.lua
@@ -1,242 +1,213 @@
-------------------------------------------------------------------------------------------
--- Inspired by https://github.com/wez/wezterm/discussions/628#discussioncomment-1874614 --
-------------------------------------------------------------------------------------------
+--
+--[[ FormatItems: Begin ]]
+---@class FormatItem.Text
+---@field Text string
 
-local wezterm = require('wezterm')
-local Cells = require('utils.cells')
+---@class FormatItem.Attribute.Intensity
+---@field Intensity 'Bold'|'Half'|'Normal'
 
-local nf = wezterm.nerdfonts
-local attr = Cells.attr
+---@class FormatItem.Attribute.Italic
+---@field Italic boolean
 
-local GLYPH_SCIRCLE_LEFT = nf.ple_left_half_circle_thick --[[  ]]
-local GLYPH_SCIRCLE_RIGHT = nf.ple_right_half_circle_thick --[[  ]]
-local GLYPH_CIRCLE = nf.fa_circle --[[  ]]
-local GLYPH_ADMIN = nf.md_shield_half_full --[[ 󰞀 ]]
-local GLYPH_LINUX = nf.cod_terminal_linux --[[  ]]
-local GLYPH_DEBUG = nf.fa_bug --[[  ]]
--- local GLYPH_SEARCH = nf.fa_search --[[  ]]
-local GLYPH_SEARCH = '🔭'
+---@class FormatItem.Attribute.Underline
+---@field Underline 'None'|'Single'|'Double'|'Curly'
 
-local TITLE_INSET = {
-  DEFAULT = 6,
-  ICON = 8,
-}
+---@class FormatItem.Attribute
+---@field Attribute FormatItem.Attribute.Intensity|FormatItem.Attribute.Italic|FormatItem.Attribute.Underline
 
-local M = {}
+---@class FormatItem.Foreground
+---@field Background {Color: string}
 
-local RENDER_VARIANTS = {
-  { 'scircle_left', 'title', 'padding',       'scircle_right' },
-  { 'scircle_left', 'title', 'unseen_output', 'padding',       'scircle_right' },
-  { 'scircle_left', 'admin', 'title',         'padding',       'scircle_right' },
-  { 'scircle_left', 'admin', 'title',         'unseen_output', 'padding',      'scircle_right' },
-  { 'scircle_left', 'wsl',   'title',         'padding',       'scircle_right' },
-  { 'scircle_left', 'wsl',   'title',         'unseen_output', 'padding',      'scircle_right' },
-}
+---@class FormatItem.Background
+---@field Foreground {Color: string}
 
----@type table<string, Cells.SegmentColors>
--- stylua: ignore
-local colors = {
-  text_default          = { bg = '#45475A', fg = '#1C1B19' },
-  text_hover            = { bg = '#587D8C', fg = '#1C1B19' },
-  text_active           = { bg = '#7FB4CA', fg = '#11111B' },
+---@alias FormatItem.Reset 'ResetAttributes'
 
-  unseen_output_default = { bg = '#45475A', fg = '#FFA066' },
-  unseen_output_hover   = { bg = '#587D8C', fg = '#FFA066' },
-  unseen_output_active  = { bg = '#7FB4CA', fg = '#FFA066' },
+---@alias FormatItem FormatItem.Text|FormatItem.Attribute|FormatItem.Foreground|FormatItem.Background|FormatItem.Reset
+--[[ FormatItems: End ]]
 
-  scircle_default       = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#45475A' },
-  scircle_hover         = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#587D8C' },
-  scircle_active        = { bg = 'rgba(0, 0, 0, 0.4)', fg = '#7FB4CA' },
-}
+local attr = {}
 
----@param proc string
-local function clean_process_name(proc)
-  local a = string.gsub(proc, '(.*[/\\])(.*)', '%2')
-  return a:gsub('%.exe$', '')
+---@param type 'Bold'|'Half'|'Normal'
+---@return {Attribute: FormatItem.Attribute.Intensity}
+attr.intensity = function(type)
+  return { Attribute = { Intensity = type } }
 end
 
----@param process_name string
----@param base_title string
----@param max_width number
----@param inset number
-local function create_title(process_name, base_title, max_width, inset)
-  local title
-
-  if process_name:len() > 0 then
-    title = process_name .. ' ~ ' .. base_title
-  else
-    title = base_title
-  end
-
-  if base_title == 'Debug' then
-    title = GLYPH_DEBUG .. ' DEBUG'
-    inset = inset - 2
-  end
-
-  if base_title:match('^InputSelector:') ~= nil then
-    title = base_title:gsub('InputSelector:', GLYPH_SEARCH)
-    inset = inset - 2
-  end
-
-  if title:len() > max_width - inset then
-    local diff = title:len() - max_width + inset
-    title = title:sub(1, title:len() - diff)
-  else
-    local padding = max_width - title:len() - inset
-    title = title .. string.rep(' ', padding)
-  end
-
-  return title
+---@return {Attribute: FormatItem.Attribute.Italic}
+attr.italic = function()
+  return { Attribute = { Italic = true } }
 end
 
----@class Tab
----@field title string
----@field cells Cells
----@field title_locked boolean
----@field is_wsl boolean
----@field is_admin boolean
----@field unseen_output boolean
----@field is_active boolean
-local Tab = {}
-Tab.__index = Tab
+---@param type 'None'|'Single'|'Double'|'Curly'
+---@return {Attribute: FormatItem.Attribute.Underline}
+attr.underline = function(type)
+  return { Attribute = { Underline = type } }
+end
 
-function Tab:new()
-  local tab = {
-    title = '',
-    cells = Cells:new(),
-    title_locked = false,
-    is_wsl = false,
-    is_admin = false,
-    unseen_output = false,
+---@alias Cells.SegmentColors {bg?: string|'UNSET', fg?: string|'UNSET'}
+
+---@class Cells.Segment
+---@field items FormatItem[]
+---@field has_bg boolean
+---@field has_fg boolean
+
+---Format item generator for `wezterm.format` (ref: <https://wezfurlong.org/wezterm/config/lua/wezterm/format.html>)
+---@class Cells
+---@field segments table<string|number, Cells.Segment>
+local Cells = {}
+Cells.__index = Cells
+
+---@class Cells.Attributes
+---@field intensity fun(type: 'Bold'|'Half'|'Normal'): {Attribute: FormatItem.Attribute.Intensity}
+---@field underline fun(type: 'None'|'Single'|'Double'|'Curly'): {Attribute: FormatItem.Attribute.Underline}
+---@field italic fun(): {Attribute: FormatItem.Attribute.Italic}
+---@overload fun(...: FormatItem.Attribute): FormatItem.Attribute[]
+Cells.attr = setmetatable(attr, {
+  __call = function(_, ...)
+    return { ... }
+  end,
+})
+
+function Cells:new()
+  return setmetatable({
+    segments = {},
+  }, self)
+end
+
+---@param segment_id string|number the segment id
+---@param text string the text to push
+---@param color? Cells.SegmentColors the bg and fg colors for text
+---@param attributes? FormatItem.Attribute[] use bold text
+function Cells:add_segment(segment_id, text, color, attributes)
+  color = color or {}
+
+  ---@type FormatItem[]
+  local items = {}
+
+  if color.bg then
+    assert(color.bg ~= 'UNSET', 'Cannot use UNSET when adding new segment')
+    table.insert(items, { Background = { Color = color.bg } })
+  end
+  if color.fg then
+    assert(color.bg ~= 'UNSET', 'Cannot use UNSET when adding new segment')
+    table.insert(items, { Foreground = { Color = color.fg } })
+  end
+  if attributes and #attributes > 0 then
+    for _, attr_ in ipairs(attributes) do
+      table.insert(items, attr_)
+    end
+  end
+  table.insert(items, { Text = text })
+  table.insert(items, 'ResetAttributes')
+
+  ---@type Cells.Segment
+  self.segments[segment_id] = {
+    items = items,
+    has_bg = color.bg ~= nil,
+    has_fg = color.fg ~= nil,
   }
-  return setmetatable(tab, self)
+
+  return self
 end
 
----@param pane any WezTerm https://wezfurlong.org/wezterm/config/lua/pane/index.html
-function Tab:set_info(pane, max_width)
-  local process_name = clean_process_name(pane.foreground_process_name)
-  self.is_wsl = process_name:match('^wsl') ~= nil
-  self.is_admin = (pane.title:match('^Administrator: ') or pane.title:match('(Admin)')) ~= nil
-  self.unseen_output = pane.has_unseen_output
-
-  if self.title_locked then
-    return
+---@private
+---@param segment_id string|number the segment id
+function Cells:_check_segment(segment_id)
+  if not self.segments[segment_id] then
+    error('Segment "' .. segment_id .. '" not found')
   end
-  local inset = (self.is_admin or self.is_wsl) and TITLE_INSET.ICON or TITLE_INSET.DEFAULT
-  if self.unseen_output then
-    inset = inset + 2
-  end
-  self.title = create_title(process_name, pane.title, max_width, inset)
 end
 
-function Tab:set_cells()
-  self.cells
-      :add_segment('scircle_left', GLYPH_SCIRCLE_LEFT)
-      :add_segment('admin', ' ' .. GLYPH_ADMIN)
-      :add_segment('wsl', ' ' .. GLYPH_LINUX)
-      :add_segment('title', ' ', nil, attr(attr.intensity('Bold')))
-      :add_segment('unseen_output', ' ' .. GLYPH_CIRCLE)
-      :add_segment('padding', ' ')
-      :add_segment('scircle_right', GLYPH_SCIRCLE_RIGHT)
+---@param segment_id string|number the segment id
+---@param text string the text to push
+function Cells:update_segment_text(segment_id, text)
+  self:_check_segment(segment_id)
+  local idx = #self.segments[segment_id].items - 1
+  self.segments[segment_id].items[idx] = { Text = text }
+  return self
 end
 
----@param title string
-function Tab:update_and_lock_title(title)
-  self.title = title
-  self.title_locked = true
-end
+---@param segment_id string|number the segment id
+---@param color Cells.SegmentColors the bg and fg colors for text
+function Cells:update_segment_colors(segment_id, color)
+  assert(type(color) == 'table', 'Color must be a table')
 
----@param is_active boolean
----@param hover boolean
-function Tab:update_cells(is_active, hover)
-  local tab_state = 'default'
-  if is_active then
-    tab_state = 'active'
-  elseif hover then
-    tab_state = 'hover'
-  end
+  self:_check_segment(segment_id)
 
-  self.cells:update_segment_text('title', ' ' .. self.title)
-  self.cells
-      :update_segment_colors('scircle_left', colors['scircle_' .. tab_state])
-      :update_segment_colors('admin', colors['text_' .. tab_state])
-      :update_segment_colors('wsl', colors['text_' .. tab_state])
-      :update_segment_colors('title', colors['text_' .. tab_state])
-      :update_segment_colors('unseen_output', colors['unseen_output_' .. tab_state])
-      :update_segment_colors('padding', colors['text_' .. tab_state])
-      :update_segment_colors('scircle_right', colors['scircle_' .. tab_state])
-end
+  local has_bg = self.segments[segment_id].has_bg
+  local has_fg = self.segments[segment_id].has_fg
 
----@return FormatItem[] (ref: https://wezfurlong.org/wezterm/config/lua/wezterm/format.html)
-function Tab:render()
-  local variant_idx = self.is_admin and 3 or 1
-  if self.is_wsl then
-    variant_idx = 5
-  end
-
-  if self.unseen_output then
-    variant_idx = variant_idx + 1
-  end
-  return self.cells:render(RENDER_VARIANTS[variant_idx])
-end
-
----@type Tab[]
-local tab_list = {}
-
-M.setup = function()
-  local enable_tab_bar = true
-
-  -- CUSTOM EVENT
-  -- Event listener to manually update the tab name
-  -- Tab name will remain locked until the `reset-tab-title` is triggered
-  wezterm.on('tabs.manual-update-tab-title', function(window, pane)
-    window:perform_action(
-      wezterm.action.PromptInputLine({
-        description = wezterm.format({
-          { Foreground = { Color = '#FFFFFF' } },
-          { Attribute = { Intensity = 'Bold' } },
-          { Text = 'Enter new name for tab' },
-        }),
-        action = wezterm.action_callback(function(_window, _pane, line)
-          if line ~= nil then
-            local tab = window:active_tab()
-            local id = tab:tab_id()
-            tab_list[id]:update_and_lock_title(line)
-          end
-        end),
-      }),
-      pane
-    )
-  end)
-
-  -- CUSTOM EVENT
-  -- Event listener to unlock manually set tab name
-  wezterm.on('tabs.reset-tab-title', function(window, _pane)
-    local tab = window:active_tab()
-    local id = tab:tab_id()
-    tab_list[id].title_locked = false
-  end)
-
-  -- CUSTOM EVENT
-  -- Event listener to manually update the tab name
-  wezterm.on('tabs.toggle-tab-bar', function(window, _pane)
-    enable_tab_bar = not enable_tab_bar
-    window:set_config_overrides({ enable_tab_bar = enable_tab_bar })
-  end)
-
-  -- BUILTIN EVENT
-  wezterm.on('format-tab-title', function(tab, _tabs, _panes, _config, hover, max_width)
-    if not tab_list[tab.tab_id] then
-      tab_list[tab.tab_id] = Tab:new()
-      tab_list[tab.tab_id]:set_info(tab.active_pane, max_width)
-      tab_list[tab.tab_id]:set_cells()
-      return tab_list[tab.tab_id]:render()
+  if color.bg then
+    if has_bg and color.bg == 'UNSET' then
+      table.remove(self.segments[segment_id].items, 1)
+      has_bg = false
+      goto bg_end
     end
 
-    tab_list[tab.tab_id]:set_info(tab.active_pane, max_width)
-    tab_list[tab.tab_id]:update_cells(tab.is_active, hover)
-    return tab_list[tab.tab_id]:render()
-  end)
+    if has_bg then
+      self.segments[segment_id].items[1] = { Background = { Color = color.bg } }
+    else
+      table.insert(self.segments[segment_id].items, 1, { Background = { Color = color.bg } })
+      has_bg = true
+    end
+  end
+  ::bg_end::
+
+  if color.fg then
+    local fg_idx = has_bg and 2 or 1
+    if has_fg and color.fg == 'UNSET' then
+      table.remove(self.segments[segment_id].items, fg_idx)
+      has_fg = false
+      goto fg_end
+    end
+
+    if has_fg then
+      self.segments[segment_id].items[fg_idx] = { Foreground = { Color = color.fg } }
+    else
+      table.insert(
+        self.segments[segment_id].items,
+        fg_idx,
+        { Foreground = { Color = color.fg } }
+      )
+      has_fg = true
+    end
+  end
+  ::fg_end::
+
+  self.segments[segment_id].has_bg = has_bg
+  self.segments[segment_id].has_fg = has_fg
+  return self
 end
 
-return M
+---@param ids table<string|number> the segment ids
+---@return FormatItem[]
+function Cells:render(ids)
+  local cells = {}
+
+  for _, id in ipairs(ids) do
+    self:_check_segment(id)
+
+    for _, item in pairs(self.segments[id].items) do
+      table.insert(cells, item)
+    end
+  end
+  return cells
+end
+
+---@return FormatItem[]
+function Cells:render_all()
+  local cells = {}
+  for _, segment in pairs(self.segments) do
+    for _, item in pairs(segment.items) do
+      table.insert(cells, item)
+    end
+  end
+  return cells
+end
+
+function Cells:reset()
+  self.segments = {}
+end
+
+return Cells
