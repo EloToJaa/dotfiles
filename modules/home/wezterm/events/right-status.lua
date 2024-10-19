0184ae20717ca1dd44ba6f73bde644a94c8ca1dc
EloToJaa <elotoja@protonmail.com>
2024-10-19 13:25:01 +0200

    Add workspace

--- modules/home/wezterm/events/right-status.lua
+++ modules/home/wezterm/events/right-status.lua
@@ -7,19 +7,37 @@ local attr = Cells.attr
 local M = {}
 
 local ICON_SEPARATOR = nf.oct_dash
-local ICON_DATE = nf.fa_calendar
+local ICON_STAT = nf.oct_table
 local ICON_DIR = nf.oct_file_directory_fill
 
 -- TODO: add windows path support
-local basename = function(s)
+local function basename(s)
   -- Nothing a little regex can't fix
   return string.match(s, "([^/]+)/?$")
+end
+
+local function get_cwd(pane)
+  local cwd = pane:get_current_working_dir()
+  if cwd then
+    cwd = basename(cwd.file_path)
+  else
+    cwd = ""
+  end
+  return cwd
+end
+
+local function get_stat(window)
+  local stat = window:active_workspace()
+  if window:active_key_table() then
+    stat = window:active_key_table()
+  end
+  return stat
 end
 
 ---@type table<string, Cells.SegmentColors>
 -- stylua: ignore
 local colors = {
-  date      = { fg = '#fab387', bg = 'rgba(0, 0, 0, 0.4)' },
+  stat      = { fg = '#f7768e', bg = 'rgba(0, 0, 0, 0.4)' },
   dir       = { fg = '#fab387', bg = 'rgba(0, 0, 0, 0.4)' },
   separator = { fg = '#74c7ec', bg = 'rgba(0, 0, 0, 0.4)' }
 }
@@ -27,29 +45,23 @@ local colors = {
 local cells = Cells:new()
 
 cells
-    :add_segment('date_icon', ICON_DATE .. '  ', colors.date, attr(attr.intensity('Bold')))
-    :add_segment('date_text', '', colors.date, attr(attr.intensity('Bold')))
+    :add_segment('stat_icon', ICON_STAT .. '  ', colors.date, attr(attr.intensity('Bold')))
+    :add_segment('stat_text', '', colors.date, attr(attr.intensity('Bold')))
     :add_segment('separator', ' ' .. ICON_SEPARATOR .. '  ', colors.separator)
     :add_segment('dir_icon', ICON_DIR .. ' ', colors.dir, attr(attr.intensity('Bold')))
     :add_segment('dir_text', '', colors.dir, attr(attr.intensity('Bold')))
 
 M.setup = function()
   wezterm.on('update-right-status', function(window, pane)
-    local cwd = pane:get_current_working_dir()
-    wezterm.log_info('cwd', cwd.file_path)
-    if cwd then
-      cwd = basename(cwd.file_path)
-    else
-      cwd = ""
-    end
-    wezterm.log_info('cwd processed', cwd)
+    local stat = get_stat(window)
+    local cwd = get_cwd(pane)
     cells
-        :update_segment_text('date_text', wezterm.strftime('%a %H:%M:%S'))
+        :update_segment_text('stat_text', stat)
         :update_segment_text('dir_text', cwd)
 
     window:set_right_status(
       wezterm.format(
-        cells:render({ 'date_icon', 'date_text', 'separator', 'dir_icon', 'dir_text' })
+        cells:render({ 'stat_icon', 'stat_text', 'separator', 'dir_icon', 'dir_text' })
       )
     )
   end)
