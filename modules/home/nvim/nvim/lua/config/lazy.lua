34f1403c2ac816dacf65f8838ca7e733c5f12a32
EloToJaa <elotoja@protonmail.com>
2024-10-20 20:27:50 +0200

    Add catppuccin theme

--- modules/home/nvim/nvim/lua/config/lazy.lua
+++ modules/home/nvim/nvim/lua/config/lazy.lua
@@ -1,4 +1,5 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
@@ -17,7 +18,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- import your plugins
-    { import = "plugins" },
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
