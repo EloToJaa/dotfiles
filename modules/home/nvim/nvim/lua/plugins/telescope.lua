976e44e156d529cfe13b80fc19b8d6cb5075ce4d
EloToJaa <elotoja@protonmail.com>
2024-10-20 22:37:00 +0200

    Update telescope keymaps

--- modules/home/nvim/nvim/lua/plugins/telescope.lua
+++ modules/home/nvim/nvim/lua/plugins/telescope.lua
@@ -7,7 +7,7 @@ return {
     local keymap = vim.keymap
     keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
     keymap.set('n', '<C-p>', builtin.find_files, { desc = 'Telescope find git files' })
-    keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+    keymap.set('n', '<leader>fs', builtin.live_grep, { desc = 'Telescope live grep' })
     keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
     keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
   end
