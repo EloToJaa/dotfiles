4fc4ad17b576c219aede90a843d14f2df10ba032
EloToJaa <elotoja@protonmail.com>
2024-10-20 21:00:48 +0200

    Restructure config

--- modules/home/nvim/nvim/lua/plugins/catppuccin.lua
+++ modules/home/nvim/nvim/lua/plugins/catppuccin.lua
@@ -1,14 +1,19 @@
-require("catppuccin").setup({
-  flavour = "auto", -- latte, frappe, macchiato, mocha
-  background = {    -- :h background
-    light = "latte",
-    dark = "mocha",
-  },
-  transparent_background = true,
-})
-
-vim.cmd.colorscheme "catppuccin"
-
 return {
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "auto", -- latte, frappe, macchiato, mocha
+        background = { -- :h background
+          light = "latte",
+          dark = "mocha",
+        },
+        transparent_background = true,
+      })
+
+      vim.cmd.colorscheme "catppuccin"
+    end
+  }
 }
