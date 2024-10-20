52c8fbc8caa29d9d275cea826c3d79a977d2e618
EloToJaa <elotoja@protonmail.com>
2024-10-20 18:24:44 +0200

    Update hyperlinks

--- modules/home/wezterm/wezterm/config/general.lua
+++ modules/home/wezterm/wezterm/config/general.lua
@@ -35,13 +35,33 @@ return {
     },
     -- Then handle URLs not wrapped in brackets
     {
-      regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-      format = '$0',
+      -- Before
+      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+      --format = '$0',
+      -- After
+      regex = '[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)',
+      format = '$1',
+      highlight = 1,
     },
     -- implicit mailto link
     {
       regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
       format = 'mailto:$0',
+    },
+
+    -- file:// URI
+    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
+    {
+      regex = [[\bfile://\S*\b]],
+      format = "$0",
+    },
+
+    -- Make username/project paths clickable. This implies paths like the following are for GitHub.
+    -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
+    -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
+    {
+      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
+      format = "https://www.github.com/$1/$3",
     },
   },
 }
