c0108c769e049cf3d18466a4dc9b781841336760
EloToJaa <elotoja@protonmail.com>
2024-10-20 20:47:36 +0200

    Add catppuccin

--- modules/home/nvim/nvim/lua/plugins/catppuccin.lua
+++ modules/home/nvim/nvim/lua/plugins/catppuccin.lua
@@ -1,3 +1,14 @@
+require("catppuccin").setup({
+  flavour = "auto", -- latte, frappe, macchiato, mocha
+  background = {    -- :h background
+    light = "latte",
+    dark = "mocha",
+  },
+  transparent_background = true,
+})
+
+vim.cmd.colorscheme "catppuccin"
+
 return {
   { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
 }
