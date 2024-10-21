45d4bf1e6322b4c5a1a1ea3defc321b6942651bc
EloToJaa <elotoja@protonmail.com>
2024-10-21 23:12:10 +0200

    Update telescope config

--- modules/home/nvim/nvim/lua/elotoja/plugins/telescope.lua
+++ modules/home/nvim/nvim/lua/elotoja/plugins/telescope.lua
@@ -7,14 +7,24 @@ return {
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
   },
   config = function()
+    local actions = require('telescope.actions')
+
     require('telescope').setup {
+      defaults = {
+        mappings = {
+          i = {
+            ['<C-k>'] = actions.move_selection_previous,
+            ['<C-j>'] = actions.move_selection_next,
+            ['<C-q>'] = actions.send_selected_to_qflist + actions.open_qflist
+          }
+        }
+      },
       extensions = {
         fzf = {
           fuzzy = true,                   -- false will only do exact matching
           override_generic_sorter = true, -- override the generic sorter
           override_file_sorter = true,    -- override the file sorter
           case_mode = 'smart_case',       -- or 'ignore_case' or 'respect_case'
-          -- the default case_mode is 'smart_case'
         }
       }
     }
