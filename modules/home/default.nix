bf3f31e5fd8636b61b5b2d5e8f81f82329dc9de4
EloToJaa <elotoja@protonmail.com>
2024-10-25 19:03:27 +0200

    Add qbittorrent to defaults.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -1,33 +1,39 @@
-{inputs, variables, host, ...}: {
+{
+  inputs,
+  variables,
+  host,
+  ...
+}: {
   imports = [
-    ./hyprland                        # window manager
-    ./nvim                            # neovim editor
-    ./ohmyposh                        # shell prompt
-    ./scripts                         # personal scripts
-    ./swaync                          # notification deamon
+    ./hyprland # window manager
+    ./nvim # neovim editor
+    ./ohmyposh # shell prompt
+    ./scripts # personal scripts
+    ./swaync # notification deamon
     ./wallpapers
-    ./waybar                          # status bar
+    ./waybar # status bar
     ./wezterm
-    ./zsh                             # shell
-    ./audacious.nix                   # music player
-    ./bat.nix                         # better cat command
-    ./btop.nix                        # resouces monitor 
+    ./zsh # shell
+    ./audacious.nix # music player
+    ./bat.nix # better cat command
+    ./btop.nix # resouces monitor
     ./catppuccin.nix
-    ./cava.nix                        # audio visualizer
-    ./discord.nix                     # discord
-    ./fastfetch.nix                   # fetch tool
-    ./fzf.nix                         # fuzzy finder
-    ./git.nix                         # version control
-    ./gnome.nix                       # gnome apps
-    ./gtk.nix                         # gtk theme
+    ./cava.nix # audio visualizer
+    ./discord.nix # discord
+    ./fastfetch.nix # fetch tool
+    ./fzf.nix # fuzzy finder
+    ./git.nix # version control
+    ./gnome.nix # gnome apps
+    ./gtk.nix # gtk theme
     ./lazygit.nix
-    ./nemo.nix                        # file manager
-    ./packages.nix                    # other packages
-    ./rofi.nix                        # launcher
-    ./spicetify.nix                   # spotify client
-    ./swayosd.nix                     # brightness / volume wiget
-    ./vscodium.nix                    # vscode fork
-    ./xdg-mimes.nix                   # xdg config
+    ./nemo.nix # file manager
+    ./packages.nix # other packages
+    ./rofi.nix # launcher
+    ./spicetify.nix # spotify client
+    ./swayosd.nix # brightness / volume wiget
+    ./vscodium.nix # vscode fork
+    ./xdg-mimes.nix # xdg config
     ./zen.nix
+    ./qbittorrent.nix
   ];
 }
