35c2603d047c81e8fef723069c17d7a401431261
EloToJaa <elotoja@protonmail.com>
2024-10-25 18:26:31 +0200

    Update wezterm bindings & disable config auto reload

--- modules/home/wezterm/wezterm/config/bindings.lua
+++ modules/home/wezterm/wezterm/config/bindings.lua
@@ -1,148 +1,150 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local keys = {
-  -- Send C-a when pressing C-a twice
-  { key = 'a',          mods = 'LEADER|CTRL', action = act.SendKey { key = 'a', mods = 'CTRL' } },
-  { key = 'c',          mods = 'LEADER',      action = act.ActivateCopyMode },
-  { key = 'phys:Space', mods = 'LEADER',      action = act.ActivateCommandPalette },
+	-- Send C-a when pressing C-a twice
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "c", mods = "LEADER", action = act.ActivateCopyMode },
+	{ key = "phys:Space", mods = "LEADER", action = act.ActivateCommandPalette },
 
-  -- Pane keybindings
-  { key = 'v',          mods = 'LEADER',      action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-  { key = 's',          mods = 'LEADER',      action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-  { key = 'h',          mods = 'LEADER',      action = act.ActivatePaneDirection('Left') },
-  { key = 'j',          mods = 'LEADER',      action = act.ActivatePaneDirection('Down') },
-  { key = 'k',          mods = 'LEADER',      action = act.ActivatePaneDirection('Up') },
-  { key = 'l',          mods = 'LEADER',      action = act.ActivatePaneDirection('Right') },
-  { key = 'LeftArrow',  mods = 'LEADER',      action = act.ActivatePaneDirection('Left') },
-  { key = 'DownArrow',  mods = 'LEADER',      action = act.ActivatePaneDirection('Down') },
-  { key = 'UpArrow',    mods = 'LEADER',      action = act.ActivatePaneDirection('Up') },
-  { key = 'RightArrow', mods = 'LEADER',      action = act.ActivatePaneDirection('Right') },
-  { key = 'x',          mods = 'LEADER',      action = act.CloseCurrentPane { confirm = false } },
-  { key = 'q',          mods = 'LEADER',      action = act.CloseCurrentTab { confirm = false } },
-  { key = 'z',          mods = 'LEADER',      action = act.TogglePaneZoomState },
-  { key = 'o',          mods = 'LEADER',      action = act.RotatePanes 'Clockwise' },
-  {
-    key = 'u',
-    mods = 'LEADER',
-    action = act.QuickSelectArgs({
-      label = 'open url',
-      patterns = {
-        '\\((https?://\\S+)\\)',
-        '\\[(https?://\\S+)\\]',
-        '\\{(https?://\\S+)\\}',
-        '<(https?://\\S+)>',
-        '\\bhttps?://\\S+[)/a-zA-Z0-9-]+'
-      },
-      action = wezterm.action_callback(function(window, pane)
-        local url = window:get_selection_text_for_pane(pane)
-        wezterm.log_info('opening: ' .. url)
-        wezterm.open_with(url)
-      end),
-    }),
-  },
-  -- We can make separate keybindings for resizing panes
-  -- But Wezterm offers custom 'mode' in the name of 'KeyTable'
-  { key = 'e', mods = 'LEADER',       action = act.ActivateKeyTable { name = 'resize_pane', one_shot = false } },
-  {
-    key = 'f',
-    mods = 'LEADER',
-    action = act.ActivateKeyTable({
-      name = 'resize_font',
-      one_shot = false,
-      timemout_miliseconds = 1000,
-    }),
-  },
+	-- Pane keybindings
+	{ key = "v", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "s", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "LeftArrow", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "DownArrow", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "UpArrow", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "RightArrow", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+	{ key = "x", mods = "LEADER", action = act.CloseCurrentPane({ confirm = false }) },
+	{ key = "q", mods = "LEADER", action = act.CloseCurrentTab({ confirm = false }) },
+	{ key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+	{ key = "o", mods = "LEADER", action = act.RotatePanes("Clockwise") },
+	{
+		key = "u",
+		mods = "LEADER",
+		action = act.QuickSelectArgs({
+			label = "open url",
+			patterns = {
+				"\\((https?://\\S+)\\)",
+				"\\[(https?://\\S+)\\]",
+				"\\{(https?://\\S+)\\}",
+				"<(https?://\\S+)>",
+				"\\bhttps?://\\S+[)/a-zA-Z0-9-]+",
+			},
+			action = wezterm.action_callback(function(window, pane)
+				local url = window:get_selection_text_for_pane(pane)
+				wezterm.log_info("opening: " .. url)
+				wezterm.open_with(url)
+			end),
+		}),
+	},
+	-- We can make separate keybindings for resizing panes
+	-- But Wezterm offers custom 'mode' in the name of 'KeyTable'
+	{ key = "e", mods = "LEADER", action = act.ActivateKeyTable({ name = "resize_pane", one_shot = false }) },
+	{
+		key = "f",
+		mods = "LEADER",
+		action = act.ActivateKeyTable({
+			name = "resize_font",
+			one_shot = false,
+			timemout_miliseconds = 1000,
+		}),
+	},
 
-  -- Tab keybindings
-  { key = 't', mods = 'LEADER',       action = act.SpawnTab('CurrentPaneDomain') },
-  { key = '[', mods = 'LEADER',       action = act.ActivateTabRelative(-1) },
-  { key = ']', mods = 'LEADER',       action = act.ActivateTabRelative(1) },
-  { key = 'n', mods = 'LEADER',       action = act.ShowTabNavigator },
+	-- Tab keybindings
+	{ key = "t", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "[", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "]", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "n", mods = "LEADER", action = act.ShowTabNavigator },
 
-  -- rename tab
-  { key = 'r', mods = 'LEADER',       action = act.EmitEvent('tabs.manual-update-tab-title') },
-  { key = 'r', mods = 'LEADER|SHIFT', action = act.EmitEvent('tabs.reset-tab-title') },
+	-- rename tab
+	{ key = "r", mods = "LEADER", action = act.EmitEvent("tabs.manual-update-tab-title") },
+	{ key = "r", mods = "LEADER|SHIFT", action = act.EmitEvent("tabs.reset-tab-title") },
 
-  -- Key table for moving tabs around
-  { key = 'm', mods = 'LEADER',       action = act.ActivateKeyTable { name = 'move_tab', one_shot = false } },
-  -- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
-  { key = '{', mods = 'LEADER|SHIFT', action = act.MoveTabRelative(-1) },
-  { key = '}', mods = 'LEADER|SHIFT', action = act.MoveTabRelative(1) },
+	-- Key table for moving tabs around
+	{ key = "m", mods = "LEADER", action = act.ActivateKeyTable({ name = "move_tab", one_shot = false }) },
+	-- Or shortcuts to move tab w/o move_tab table. SHIFT is for when caps lock is on
+	{ key = "{", mods = "LEADER|SHIFT", action = act.MoveTabRelative(-1) },
+	{ key = "}", mods = "LEADER|SHIFT", action = act.MoveTabRelative(1) },
 
-  -- Lastly, workspace
-  { key = 'w', mods = 'LEADER',       action = act.ShowLauncherArgs { flags = 'FUZZY|WORKSPACES' } },
+	-- Lastly, workspace
+	{ key = "w", mods = "LEADER", action = act.ShowLauncherArgs({ flags = "FUZZY|WORKSPACES" }) },
 
-  { key = 'c', mods = 'CTRL|SHIFT',   action = act.CopyTo('Clipboard') },
-  { key = 'v', mods = 'CTRL|SHIFT',   action = act.PasteFrom('Clipboard') },
+	{ key = "c", mods = "LEADER", action = act.ReloadConfiguration },
+
+	{ key = "c", mods = "CTRL|SHIFT", action = act.CopyTo("Clipboard") },
+	{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
 }
 
 for i = 1, 9 do
-  table.insert(keys, {
-    key = tostring(i),
-    mods = 'LEADER',
-    action = act.ActivateTab(i - 1)
-  })
+	table.insert(keys, {
+		key = tostring(i),
+		mods = "LEADER",
+		action = act.ActivateTab(i - 1),
+	})
 end
 table.insert(keys, {
-  key = '0',
-  mods = 'LEADER',
-  action = act.ActivateTab(9)
+	key = "0",
+	mods = "LEADER",
+	action = act.ActivateTab(9),
 })
 
 local key_tables = {
-  resize_font = {
-    { key = 'k',         action = act.IncreaseFontSize },
-    { key = 'j',         action = act.DecreaseFontSize },
-    { key = 'UpArrow',   action = act.IncreaseFontSize },
-    { key = 'DownArrow', action = act.DecreaseFontSize },
-    { key = 'r',         action = act.ResetFontSize },
-    { key = 'Escape',    action = 'PopKeyTable' },
-    { key = 'q',         action = 'PopKeyTable' },
-  },
-  resize_pane = {
-    { key = 'h',          action = act.AdjustPaneSize { 'Left', 1 } },
-    { key = 'j',          action = act.AdjustPaneSize { 'Down', 1 } },
-    { key = 'k',          action = act.AdjustPaneSize { 'Up', 1 } },
-    { key = 'l',          action = act.AdjustPaneSize { 'Right', 1 } },
-    { key = 'LeftArrow',  action = act.AdjustPaneSize { 'Left', 1 } },
-    { key = 'DownArrow',  action = act.AdjustPaneSize { 'Down', 1 } },
-    { key = 'UpArrow',    action = act.AdjustPaneSize { 'Up', 1 } },
-    { key = 'RightArrow', action = act.AdjustPaneSize { 'Right', 1 } },
-    { key = 'Escape',     action = 'PopKeyTable' },
-    { key = 'q',          action = 'PopKeyTable' },
-  },
-  move_tab = {
-    { key = 'h',          action = act.MoveTabRelative(-1) },
-    { key = 'j',          action = act.MoveTabRelative(-1) },
-    { key = 'k',          action = act.MoveTabRelative(1) },
-    { key = 'l',          action = act.MoveTabRelative(1) },
-    { key = 'LeftArrow',  action = act.MoveTabRelative(-1) },
-    { key = 'DownArrow',  action = act.MoveTabRelative(-1) },
-    { key = 'UpArrow',    action = act.MoveTabRelative(1) },
-    { key = 'RightArrow', action = act.MoveTabRelative(1) },
-    { key = 'Escape',     action = 'PopKeyTable' },
-    { key = 'q',          action = 'PopKeyTable' },
-  }
+	resize_font = {
+		{ key = "k", action = act.IncreaseFontSize },
+		{ key = "j", action = act.DecreaseFontSize },
+		{ key = "UpArrow", action = act.IncreaseFontSize },
+		{ key = "DownArrow", action = act.DecreaseFontSize },
+		{ key = "r", action = act.ResetFontSize },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "q", action = "PopKeyTable" },
+	},
+	resize_pane = {
+		{ key = "h", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "j", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "k", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "l", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "LeftArrow", action = act.AdjustPaneSize({ "Left", 1 }) },
+		{ key = "DownArrow", action = act.AdjustPaneSize({ "Down", 1 }) },
+		{ key = "UpArrow", action = act.AdjustPaneSize({ "Up", 1 }) },
+		{ key = "RightArrow", action = act.AdjustPaneSize({ "Right", 1 }) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "q", action = "PopKeyTable" },
+	},
+	move_tab = {
+		{ key = "h", action = act.MoveTabRelative(-1) },
+		{ key = "j", action = act.MoveTabRelative(-1) },
+		{ key = "k", action = act.MoveTabRelative(1) },
+		{ key = "l", action = act.MoveTabRelative(1) },
+		{ key = "LeftArrow", action = act.MoveTabRelative(-1) },
+		{ key = "DownArrow", action = act.MoveTabRelative(-1) },
+		{ key = "UpArrow", action = act.MoveTabRelative(1) },
+		{ key = "RightArrow", action = act.MoveTabRelative(1) },
+		{ key = "Escape", action = "PopKeyTable" },
+		{ key = "q", action = "PopKeyTable" },
+	},
 }
 -- remove copy on highlight
 local mouse_bindings = {
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'NONE',
-    action = wezterm.action.Nop,
-  },
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'CTRL',
-    action = act.OpenLinkAtMouseCursor,
-  },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = wezterm.action.Nop,
+	},
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = act.OpenLinkAtMouseCursor,
+	},
 }
 
 return {
-  disable_default_key_bindings = true,
-  leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 },
-  keys = keys,
-  key_tables = key_tables,
-  mouse_bindings = mouse_bindings,
+	disable_default_key_bindings = true,
+	leader = { key = "a", mods = "CTRL", timeout_milliseconds = 2000 },
+	keys = keys,
+	key_tables = key_tables,
+	mouse_bindings = mouse_bindings,
 }
--- modules/home/wezterm/wezterm/config/general.lua
+++ modules/home/wezterm/wezterm/config/general.lua
@@ -1,67 +1,59 @@
 return {
-  -- behaviours
-  automatically_reload_config = true,
-  exit_behavior = 'CloseOnCleanExit', -- if the shell program exited with a successful status
-  exit_behavior_messaging = 'Verbose',
-  status_update_interval = 1000,
-  default_workspace = 'main',
+	-- behaviours
+	automatically_reload_config = false,
+	exit_behavior = "CloseOnCleanExit", -- if the shell program exited with a successful status
+	exit_behavior_messaging = "Verbose",
+	status_update_interval = 1000,
+	default_workspace = "main",
 
-  scrollback_lines = 10000,
+	scrollback_lines = 10000,
 
-  hyperlink_rules = {
-    -- Matches: a URL in parens: (URL)
-    {
-      regex = '\\((\\w+://\\S+)\\)',
-      format = '$1',
-      highlight = 1,
-    },
-    -- Matches: a URL in brackets: [URL]
-    {
-      regex = '\\[(\\w+://\\S+)\\]',
-      format = '$1',
-      highlight = 1,
-    },
-    -- Matches: a URL in curly braces: {URL}
-    {
-      regex = '\\{(\\w+://\\S+)\\}',
-      format = '$1',
-      highlight = 1,
-    },
-    -- Matches: a URL in angle brackets: <URL>
-    {
-      regex = '<(\\w+://\\S+)>',
-      format = '$1',
-      highlight = 1,
-    },
-    -- Then handle URLs not wrapped in brackets
-    {
-      -- Before
-      --regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
-      --format = '$0',
-      -- After
-      regex = '[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)',
-      format = '$1',
-      highlight = 1,
-    },
-    -- implicit mailto link
-    {
-      regex = '\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b',
-      format = 'mailto:$0',
-    },
+	hyperlink_rules = {
+		-- Matches: a URL in parens: (URL)
+		{
+			regex = "\\((\\w+://\\S+)\\)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in brackets: [URL]
+		{
+			regex = "\\[(\\w+://\\S+)\\]",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in curly braces: {URL}
+		{
+			regex = "\\{(\\w+://\\S+)\\}",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Matches: a URL in angle brackets: <URL>
+		{
+			regex = "<(\\w+://\\S+)>",
+			format = "$1",
+			highlight = 1,
+		},
+		-- Then handle URLs not wrapped in brackets
+		{
+			-- Before
+			--regex = '\\b\\w+://\\S+[)/a-zA-Z0-9-]+',
+			--format = '$0',
+			-- After
+			regex = "[^(]\\b(\\w+://\\S+[)/a-zA-Z0-9-]+)",
+			format = "$1",
+			highlight = 1,
+		},
+		-- implicit mailto link
+		{
+			regex = "\\b\\w+@[\\w-]+(\\.[\\w-]+)+\\b",
+			format = "mailto:$0",
+		},
 
-    -- file:// URI
-    -- Compiled-in default. Used if you don't specify any hyperlink_rules.
-    {
-      regex = [[\bfile://\S*\b]],
-      format = "$0",
-    },
-
-    -- Make username/project paths clickable. This implies paths like the following are for GitHub.
-    -- As long as a full URL hyperlink regex exists above this it should not match a full URL to
-    -- GitHub or GitLab / BitBucket (i.e. https://gitlab.com/user/project.git is still a whole clickable URL)
-    {
-      regex = [[["]?([\w\d]{1}[-\w\d]+)(/){1}([-\w\d\.]+)["]?]],
-      format = "https://www.github.com/$1/$3",
-    },
-  },
+		-- file:// URI
+		-- Compiled-in default. Used if you don't specify any hyperlink_rules.
+		{
+			regex = [[\bfile://\S*\b]],
+			format = "$0",
+		},
+	},
 }
