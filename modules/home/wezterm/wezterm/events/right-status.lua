4b9f125e0c8f9255199ec130d4cb8c79d385d95e
EloToJaa <elotoja@protonmail.com>
2024-10-25 20:08:19 +0200

    Remove dir in wezterm right-status

--- modules/home/wezterm/wezterm/events/right-status.lua
+++ modules/home/wezterm/wezterm/events/right-status.lua
@@ -1,5 +1,5 @@
-local wezterm = require('wezterm')
-local Cells = require('utils.cells')
+local wezterm = require("wezterm")
+local Cells = require("utils.cells")
 
 local nf = wezterm.nerdfonts
 local attr = Cells.attr
@@ -7,33 +7,16 @@ local attr = Cells.attr
 local M = {}
 
 local ICON_STAT = nf.oct_table
-local ICON_DIR = nf.oct_file_directory_fill
 local ICON_USER = nf.oct_person_fill
 
 local function get_username()
-  return os.getenv('USER') or os.getenv('LOGNAME') or os.getenv('USERNAME')
-end
-
----@param cwd any
----@return string
-local function convert_cwd(cwd)
-  if cwd then
-    local new_cwd = string.gsub(cwd.file_path, "^/home/" .. get_username(), "~")
-    if string.sub(new_cwd, -1) == "/" then
-      return string.sub(new_cwd, 1, -2)
-    else
-      return new_cwd
-    end
-  else
-    return ''
-  end
+	return os.getenv("USER") or os.getenv("LOGNAME") or os.getenv("USERNAME")
 end
 
 ---@type table<string, Cells.SegmentColors>
 -- stylua: ignore
 local colors = {
   stat      = { fg = '#f7768e', bg = 'rgba(0, 0, 0, 0.4)' },
-  dir       = { fg = '#fab387', bg = 'rgba(0, 0, 0, 0.4)' },
   user      = { fg = '#89dceb', bg = 'rgba(0, 0, 0, 0.4)' },
   separator = { fg = '#74c7ec', bg = 'rgba(0, 0, 0, 0.4)' }
 }
@@ -41,30 +24,25 @@ local colors = {
 local cells = Cells:new()
 
 cells
-    :add_segment('stat_icon', ICON_STAT .. '  ', colors.date, attr(attr.intensity('Bold')))
-    :add_segment('stat_text', '', colors.date, attr(attr.intensity('Bold')))
-    :add_segment('separator', ' ', colors.separator)
-    :add_segment('dir_icon', ICON_DIR .. ' ', colors.dir, attr(attr.intensity('Bold')))
-    :add_segment('dir_text', '', colors.dir, attr(attr.intensity('Bold')))
-    :add_segment('user_icon', ICON_USER .. ' ', colors.user, attr(attr.intensity('Bold')))
-    :add_segment('user_text', '', colors.user, attr(attr.intensity('Bold')))
+	:add_segment("stat_icon", ICON_STAT .. "  ", colors.date, attr(attr.intensity("Bold")))
+	:add_segment("stat_text", "", colors.date, attr(attr.intensity("Bold")))
+	:add_segment("separator", " ", colors.separator)
+	:add_segment("user_icon", ICON_USER .. " ", colors.user, attr(attr.intensity("Bold")))
+	:add_segment("user_text", "", colors.user, attr(attr.intensity("Bold")))
 
 M.setup = function()
-  wezterm.on('update-right-status', function(window, pane)
-    local stat = window:active_workspace()
-    local cwd = convert_cwd(pane:get_current_working_dir())
-    cells
-        :update_segment_text('stat_text', stat)
-        :update_segment_text('dir_text', cwd)
-        :update_segment_text('user_text', get_username())
+	wezterm.on("update-right-status", function(window, pane)
+		local stat = window:active_workspace()
+		cells:update_segment_text("stat_text", stat):update_segment_text("user_text", get_username())
 
-    window:set_right_status(
-      wezterm.format(
-        cells:render({ 'dir_icon', 'dir_text', 'separator', 'user_icon', 'user_text', 'separator', 'stat_icon',
-          'stat_text' })
-      )
-    )
-  end)
+		window:set_right_status(wezterm.format(cells:render({
+			"user_icon",
+			"user_text",
+			"separator",
+			"stat_icon",
+			"stat_text",
+		})))
+	end)
 end
 
 return M
