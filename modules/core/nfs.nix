dc41e0ecf4469eee0675a5283cb38829f25967ce
EloToJaa <elotoja@protonmail.com>
2024-10-26 13:53:48 +0200

    Add ISO share

--- modules/core/nfs.nix
+++ modules/core/nfs.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  variables,
-  ...
-}: {
+{variables, ...}: {
   fileSystems."/mnt/Data" = {
     device = "${variables.nfs}:/mnt/Main/Data";
     fsType = "nfs";
@@ -20,6 +16,11 @@
   };
   fileSystems."/mnt/Media" = {
     device = "${variables.nfs}:/mnt/Main/Media";
+    fsType = "nfs";
+    options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=90"];
+  };
+  fileSystems."/mnt/ISO" = {
+    device = "${variables.nfs}:/mnt/Main/ISO";
     fsType = "nfs";
     options = ["x-systemd.automount" "noauto" "x-systemd.after=network-online.target" "x-systemd.mount-timeout=90"];
   };
