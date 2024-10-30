aed783decbd2008b5bdb06a46a4ba6d0fd060e7c
EloToJaa <elotoja@protonmail.com>
2024-10-30 18:10:41 +0100

    Add new nvim plugins

--- modules/home/nvim/nvim/lua/elotoja/plugins/treesitter.lua
+++ modules/home/nvim/nvim/lua/elotoja/plugins/treesitter.lua
@@ -1,15 +1,48 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    local configs = require('nvim-treesitter.configs')
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+	},
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      ensure_installed = { 'vim', 'vimdoc', 'c', 'lua', 'javascript', 'typescript', 'html', 'zig', 'go' },
-      sync_install = false,
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
+		configs.setup({
+			ensure_installed = {
+				"vim",
+				"vimdoc",
+				"c",
+				"lua",
+				"javascript",
+				"typescript",
+				"html",
+				"zig",
+				"go",
+				"json",
+				"bash",
+				"yaml",
+				"toml",
+				"tsx",
+				"svelte",
+				"astro",
+				"markdown",
+				"markdown_inline",
+				"dockerfile",
+				"gitignore",
+			},
+			sync_install = false,
+			auto_install = true,
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
