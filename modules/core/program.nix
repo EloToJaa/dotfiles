4b4fed3e6479564946b9a3d4d9bc242aba4b7784
EloToJaa <elotoja@protonmail.com>
2024-11-01 03:04:59 +0100

    Update programs in core

--- modules/core/program.nix
+++ modules/core/program.nix
@@ -1,15 +1,11 @@
-{
-  pkgs,
-  lib,
-  ...
-}: {
-  programs.dconf.enable = true;
-  programs.zsh.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-    # pinentryFlavor = "";
+{...}: {
+  programs = {
+    dconf.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
+      # pinentryFlavor = "";
+    };
+    nix-ld.enable = true;
   };
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [];
 }
