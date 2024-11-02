5be03945db953bb91ca1da20bd81be2cc6ef77db
EloToJaa <elotoja@protonmail.com>
2024-11-02 17:17:32 +0100

    Add oh-my-posh to nushell

--- modules/home/ohmyposh/default.nix
+++ modules/home/ohmyposh/default.nix
@@ -1,14 +1,18 @@
-{...}: {
-  programs = {
-    oh-my-posh = {
-      enable = true;
+{pkgs, ...}: {
+  home.packages = with pkgs; [oh-my-posh];
 
-      #enableZshIntegration = true;
-      #enableNushellIntegration = true;
-    };
+  programs = {
     zsh.initExtra = ''
       eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
     '';
+    nushell = {
+      extraEnv = ''
+        oh-my-posh init nu
+      '';
+      extraConfig = ''
+        source ~/.oh-my-posh.nu
+      '';
+    };
   };
 
   xdg.configFile."oh-my-posh/config.toml".source = ./config.toml;
