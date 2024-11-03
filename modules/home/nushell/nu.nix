bb4d2bfc9d70e33865606ba317ceafc025cec793
EloToJaa <elotoja@protonmail.com>
2024-11-03 10:31:07 +0100

    Add extraConfig to nushell

--- modules/home/nushell/nu.nix
+++ modules/home/nushell/nu.nix
@@ -7,7 +7,7 @@
     enable = true;
     environmentVariables = {
       PROMPT_INDICATOR_VI_INSERT = "\"  \"";
-      #PROMPT_INDICATOR_VI_NORMAL = "\"∙ \"";
+      PROMPT_INDICATOR_VI_NORMAL = "\". \"";
       PROMPT_COMMAND = ''""'';
       PROMPT_COMMAND_RIGHT = ''""'';
       NIXPKGS_ALLOW_UNFREE = "1";
@@ -16,5 +16,49 @@
       EDITOR = config.home.sessionVariables.EDITOR;
       VISUAL = config.home.sessionVariables.VISUAL;
     };
+    extraConfig = let
+      conf = builtins.toJSON {
+        show_banner = false;
+        edit_mode = "vi";
+
+        ls.clickable_links = true;
+        rm.always_trash = true;
+
+        table = {
+          mode = "compact"; # compact thin rounded
+          index_mode = "always"; # alway never auto
+          header_on_separator = false;
+        };
+
+        cursor_shape = {
+          vi_insert = "line";
+          vi_normal = "block";
+        };
+
+        menus = [
+          {
+            name = "completion_menu";
+            only_buffer_difference = false;
+            marker = "? ";
+            type = {
+              layout = "columnar"; # list, description
+              columns = 4;
+              col_padding = 2;
+            };
+            style = {
+              text = "magenta";
+              selected_text = "blue_reverse";
+              description_text = "yellow";
+            };
+          }
+        ];
+      };
+    in ''
+      $env.config = ${conf};
+
+      alias pueue = ${pkgs.pueue}/bin/pueue
+      alias pueued = ${pkgs.pueue}/bin/pueued
+      use ${pkgs.nu_scripts}/share/nu_scripts/modules/background_task/task.nu
+    '';
   };
 }
