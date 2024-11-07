f30a2b9f39368bf4f9647320650758bc8c97a7e1
EloToJaa <elotoja@protonmail.com>
2024-11-07 21:23:39 +0100

    Increase wezterm fps

--- modules/desktop/wezterm/wezterm/config/appearance.lua
+++ modules/desktop/wezterm/wezterm/config/appearance.lua
@@ -2,8 +2,8 @@ local gpu_adapters = require("utils.gpu_adapter")
 -- local colors = require('colors.custom')
 
 return {
-	animation_fps = 60,
-	max_fps = 60,
+	animation_fps = 180,
+	max_fps = 180,
 	front_end = "WebGpu",
 	webgpu_power_preference = "HighPerformance",
 	webgpu_preferred_adapter = gpu_adapters:pick_best(),
