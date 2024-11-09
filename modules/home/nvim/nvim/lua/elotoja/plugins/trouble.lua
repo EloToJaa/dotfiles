2e32579f85bacf9cd400f7316a1a28d4f1d13a14
EloToJaa <elotoja@protonmail.com>
2024-11-09 15:53:33 +0100

    Add line wrap in trouble

--- modules/home/nvim/nvim/lua/elotoja/plugins/trouble.lua
+++ modules/home/nvim/nvim/lua/elotoja/plugins/trouble.lua
@@ -1,15 +1,37 @@
 return {
-  "folke/trouble.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
-  opts = {
-    focus = true,
-  },
-  cmd = "Trouble",
-  keys = {
-    { "<leader>xw", "<cmd>Trouble diagnostics toggle<CR>",              desc = "Open trouble workspace diagnostics" },
-    { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<CR>", desc = "Open trouble document diagnostics" },
-    { "<leader>xq", "<cmd>Trouble quickfix toggle<CR>",                 desc = "Open trouble quickfix list" },
-    { "<leader>xl", "<cmd>Trouble loclist toggle<CR>",                  desc = "Open trouble location list" },
-    { "<leader>xt", "<cmd>Trouble todo toggle<CR>",                     desc = "Open todos in trouble" },
-  },
+	"folke/trouble.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons", "folke/todo-comments.nvim" },
+	opts = {
+		focus = true,
+		auto_open = true,
+		auto_close = true,
+		use_diagnostic_signs = true,
+		-- Set up autocommand for wrapping in Trouble buffers
+		setup = function()
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "Trouble",
+				callback = function()
+					vim.opt_local.wrap = true
+					vim.opt_local.linebreak = true -- Avoid breaking words in the middle
+					vim.opt_local.breakindent = true
+				end,
+			})
+		end,
+	},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xw",
+			"<cmd>Trouble diagnostics toggle<CR>",
+			desc = "Open trouble workspace diagnostics",
+		},
+		{
+			"<leader>xd",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+			desc = "Open trouble document diagnostics",
+		},
+		{ "<leader>xq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+		{ "<leader>xl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+		{ "<leader>xt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+	},
 }
