0070880f380ead78a89c4e5fe3763c3c7e3ae5aa
EloToJaa <elotoja@protonmail.com>
2024-11-21 17:25:38 +0100

    Format shell files

--- modules/desktop/scripts/scripts/random-wallpaper.sh
+++ modules/desktop/scripts/scripts/random-wallpaper.sh
@@ -10,11 +10,11 @@ wallpaper_list=($(ls "$wallpapers_folder"))
 wallpaper_count=${#wallpaper_list[@]}
 
 while true; do
-    wallpaper_name="${wallpaper_list[RANDOM % wallpaper_count]}"
+	wallpaper_name="${wallpaper_list[RANDOM % wallpaper_count]}"
 
-    if [[ "$wallpaper_name" != "$current_wallpaper_name" ]]; then
-        break
-    fi
+	if [[ "$wallpaper_name" != "$current_wallpaper_name" ]]; then
+		break
+	fi
 done
 
 ln -sf "$wallpapers_folder/$wallpaper_name" "$wallpaper_path/wallpaper"
--- modules/desktop/scripts/scripts/wall-change.sh
+++ modules/desktop/scripts/scripts/wall-change.sh
@@ -4,7 +4,7 @@ animations=("fade" "outer" "center" "any")
 random_animation=${animations[RANDOM % ${#animations[@]}]}
 
 if [[ "$random_animation" == "wipe" ]]; then
-  swww img --transition-type="wipe" --transition-angle=135 $1 &
+	swww img --transition-type="wipe" --transition-angle=135 $1 &
 else
-  swww img --transition-type="$random_animation" $1 &
+	swww img --transition-type="$random_animation" $1 &
 fi
