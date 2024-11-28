2c04fd57d069ebf3c6354436d47162316fa615a6
EloToJaa <elotoja@protonmail.com>
2024-11-28 20:08:59 +0100

    Add set cap_net service for docker

--- modules/base/docker.nix
+++ modules/base/docker.nix
@@ -11,4 +11,14 @@
   environment.systemPackages = with pkgs; [
     rootlesskit
   ];
+  systemd.services.set-cap-rootlesskit = {
+    description = "Set CAP_NET_BIND_SERVICE on rootlesskit binary";
+    after = ["docker.service"];
+    wantedBy = ["multi-user.target"];
+    serviceConfig = {
+      ExecStart = "${pkgs.setcap}/bin/setcap cap_net_bind_service=ep ${pkgs.rootlesskit}/bin/rootlesskit";
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+  };
 }
