48f4d4349ebb62ddae8e5cc27a948d6fff385c14
EloToJaa <elotoja@protonmail.com>
2024-11-28 18:50:02 +0100

    Add static IP for desktop

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -10,5 +10,12 @@
     ./../../modules/desktop
   ];
 
+  networking.interfaces.eth0.ipv4.addresses = [
+    {
+      address = "192.168.0.20";
+      prefixLength = 24;
+    }
+  ];
+
   powerManagement.cpuFreqGovernor = "performance";
 }
