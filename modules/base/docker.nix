ec2b2ac08a8852f44ec5fa7a52df451e83723919
EloToJaa <elotoja@protonmail.com>
2024-11-30 20:46:42 +0100

    Remove podman

--- modules/base/docker.nix
+++ modules/base/docker.nix
@@ -1,33 +1,4 @@
-{
-  pkgs,
-  variables,
-  ...
-}: {
-  # virtualisation.docker.enable = true;
-  # virtualisation.docker.rootless = {
-  #   enable = true;
-  #   setSocketVariable = true;
-  # };
-  # users.users.${variables.username}.extraGroups = ["docker"];
-  security.wrappers.podman-rootlesskit = {
-    owner = "root";
-    group = "root";
-    capabilities = "cap_net_bind_service+ep";
-    source = "${pkgs.rootlesskit}/bin/rootlesskit";
-  };
-  virtualisation = {
-    containers.enable = true;
-    podman = {
-      enable = true;
-      dockerCompat = true;
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-  users.users.${variables.username} = {
-    isNormalUser = true;
-    extraGroups = ["podman"];
-  };
-  # environment.systemPackages = with pkgs; [
-  #   podman-compose
-  # ];
+{variables, ...}: {
+  virtualisation.docker.enable = true;
+  users.users.${variables.username}.extraGroups = ["docker"];
 }
