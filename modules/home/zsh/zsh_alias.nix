4c31c1bf195074305d1f6b7a9defac2b7c07df63
EloToJaa <elotoja@protonmail.com>
2024-12-11 21:33:06 +0100

    Update eza ll alias

--- modules/home/zsh/zsh_alias.nix
+++ modules/home/zsh/zsh_alias.nix
@@ -22,7 +22,7 @@
       y = "yy";
 
       l = "eza --icons  -a --group-directories-first -1"; #EZA_ICON_SPACING=2
-      ll = "eza --icons  -a --group-directories-first -1 --no-user --long";
+      ll = "eza --icons  -a --group-directories-first -1 --long -g";
       tree = "eza --icons --tree --group-directories-first";
 
       # Nixos
