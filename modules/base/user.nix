ba274828002179408cd9007d64516db65a0738f2
EloToJaa <elotoja@protonmail.com>
2024-12-14 18:22:54 +0100

    Add backupFileExt

--- modules/base/user.nix
+++ modules/base/user.nix
@@ -7,6 +7,7 @@
 }: {
   imports = [inputs.home-manager.nixosModules.home-manager];
   home-manager = {
+    backupFileExtension = "backup";
     useUserPackages = true;
     useGlobalPkgs = true;
     extraSpecialArgs = {inherit inputs variables host;};
