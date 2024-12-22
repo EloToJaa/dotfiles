1ccb59fd5fef8b3f7f35340b9f5e7cff0d6aab8b
EloToJaa <elotoja@protonmail.com>
2024-12-22 22:10:54 +0100

    Disable resolv.conf

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -1,13 +1,13 @@
 {pkgs, ...}: {
-  services.mullvad-vpn = {
-    enable = true;
-    package = pkgs.mullvad-vpn;
-  };
-  /*
-    Required to get DNS working with mullvad.
-  * https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/15
-  */
-  networking.resolvconf.enable = false;
+  # services.mullvad-vpn = {
+  #   enable = true;
+  #   package = pkgs.mullvad-vpn;
+  # };
+  # /*
+  #   Required to get DNS working with mullvad.
+  # * https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/15
+  # */
+  # networking.resolvconf.enable = false;
 
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
