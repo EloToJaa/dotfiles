797b6194bdd68127730cb86dbd42518ade709dd9
EloToJaa <elotoja@protonmail.com>
2024-12-31 13:45:34 +0100

    Add nushell for yazi

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -21,6 +21,18 @@
     }
   '';
 
+  programs.nushell.extraEnv = ''
+    def --env y [...args] {
+     let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+     yazi ...$args --cwd-file $tmp
+     let cwd = (open $tmp)
+     if $cwd != "" and $cwd != $env.PWD {
+      cd $cwd
+     }
+     rm -fp $tmp
+    }
+  '';
+
   # catppuccin.yazi = {
   #   enable = true;
   #   flavor = "${variables.catppuccin.flavor}";
