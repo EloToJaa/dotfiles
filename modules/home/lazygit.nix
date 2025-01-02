c10f0c053e70dcda5f2f9f93771feab8865d3d5c
EloToJaa <elotoja@protonmail.com>
2025-01-02 17:20:50 +0100

    Update lazygit config

--- modules/home/lazygit.nix
+++ modules/home/lazygit.nix
@@ -1,4 +1,5 @@
 {variables, ...}: {
+  # https://github.com/catppuccin/lazygit/blob/main/themes/mocha/blue.yml
   programs.lazygit = {
     enable = true;
     settings = {
@@ -7,6 +8,39 @@
           colorArgs = "always";
           pager = "delta --dark --paging=never";
         };
+      };
+      theme = {
+        activeBorderColor = [
+          "#89b4fa"
+          "bold"
+        ];
+        inactiveBorderColor = [
+          "#a6adc8"
+        ];
+        optionsTextColor = [
+          "#89b4fa"
+        ];
+        selectedLineBgColor = [
+          "#313244"
+        ];
+        cherryPickedCommitBgColor = [
+          "#45475a"
+        ];
+        cherryPickedCommitFgColor = [
+          "#89b4fa"
+        ];
+        unstagedChangesColor = [
+          "#f38ba8"
+        ];
+        defaultFgColor = [
+          "#cdd6f4"
+        ];
+        searchingActiveBorderColor = [
+          "#f9e2af"
+        ];
+      };
+      authorColors = {
+        "*" = "#b4befe";
       };
     };
   };
