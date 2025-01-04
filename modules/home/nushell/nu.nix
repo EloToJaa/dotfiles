1bc9433a23fc18be432f1ea0dd1ac0c04b943025
EloToJaa <elotoja@protonmail.com>
2025-01-04 23:54:29 +0100

    Move variables

--- modules/home/nushell/nu.nix
+++ modules/home/nushell/nu.nix
@@ -15,8 +15,8 @@ in {
       PROMPT_INDICATOR_VI_NORMAL = "";
       PROMPT_COMMAND = ''""'';
       PROMPT_COMMAND_RIGHT = ''""'';
-      NIXPKGS_ALLOW_UNFREE = "1";
-      NIXPKGS_ALLOW_INSECURE = "1";
+      NIXPKGS_ALLOW_UNFREE = config.home.sessionVariables.NIXPKGS_ALLOW_UNFREE;
+      NIXPKGS_ALLOW_INSECURE = config.home.sessionVariables.NIXPKGS_ALLOW_INSECURE;
       SHELL = ''"${pkgs.nushell}/bin/nu"'';
       EDITOR = config.home.sessionVariables.EDITOR;
       VISUAL = config.home.sessionVariables.VISUAL;
---
+++ modules/home/variables.nix
@@ -0,0 +1,8 @@
+{...}: {
+  home.sessionVariables = {
+    SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+    NIXPKGS_ALLOW_UNFREE = "1";
+    NIXPKGS_ALLOW_INSECURE = "1";
+    DOCKER_HOST = "unix:///run/user/1000/docker.sock";
+  };
+}
