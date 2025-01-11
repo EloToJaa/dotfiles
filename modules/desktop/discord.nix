7deec49491986b73f7a076c7c3e3aabb1db4f479
EloToJaa <elotoja@protonmail.com>
2025-01-11 21:36:57 +0100

    Remove ghostty input

--- modules/desktop/discord.nix
+++ modules/desktop/discord.nix
@@ -9,8 +9,8 @@
     # (discord.override {
     #  withVencord = true;
     # })
-    # webcord-vencord
-    inputs.webcord.legacyPackages.${pkgs.system}.webcord-vencord
+    webcord-vencord
+    # inputs.webcord.legacyPackages.${pkgs.system}.webcord-vencord
   ];
 
   xdg.configFile."Vencord/themes/custom.css".text = ''
