e0cbca373c30179068681f9ed4c69efa983374ec
EloToJaa <elotoja@protonmail.com>
2025-01-12 00:10:37 +0100

    Enable resolvconf

--- modules/core/network.nix
+++ modules/core/network.nix
@@ -7,7 +7,7 @@
     Required to get DNS working with mullvad.
   * https://discourse.nixos.org/t/connected-to-mullvadvpn-but-no-internet-connection/35803/15
   */
-  networking.resolvconf.enable = false;
+  # networking.resolvconf.enable = false;
 
   environment.systemPackages = with pkgs; [
     networkmanagerapplet
