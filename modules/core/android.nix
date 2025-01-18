413c339c3518dc66da6391673726b9ee4e8846ae
EloToJaa <elotoja@protonmail.com>
2025-01-18 21:18:55 +0100

    Fix android

--- modules/core/android.nix
+++ modules/core/android.nix
@@ -6,6 +6,8 @@
   programs.adb.enable = true;
   users.users.${variables.username} = {
     extraGroups = ["adbusers"];
+  };
+  home-manager.users.${variables.username} = {
     home.packages = with pkgs; [android-studio];
   };
 }
