8c0bd081f134824aa014cf6020b7f6968bea79ba
EloToJaa <elotoja@protonmail.com>
2025-01-26 18:29:20 +0100

    Add pihole

--- modules/base/network.nix
+++ modules/base/network.nix
@@ -2,7 +2,7 @@
   networking = {
     hostName = "${host}";
     networkmanager.enable = true;
-    nameservers = ["1.1.1.1" "1.0.0.1"];
+    nameservers = ["192.168.0.31" "1.1.1.1" "1.0.0.1"];
   };
   services.tailscale.enable = true;
 }
