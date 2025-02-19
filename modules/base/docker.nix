ad0327d084897af100ae0515754f9799b557dc84
EloToJaa <elotoja@protonmail.com>
2025-02-19 23:24:43 +0100

    Add daemon options

--- modules/base/docker.nix
+++ modules/base/docker.nix
@@ -8,6 +8,15 @@
     rootless = {
       enable = true;
       setSocketVariable = false;
+      daemon.settings = {
+        debug = true;
+        "log-driver" = "loki";
+        "log-opts" = {
+          "loki-url" = "https://loki.local.elotoja.com/loki/api/v1/push";
+          "loki-batch-size" = "400";
+          "loki-external-labels" = "container_name={{.Name}},hostname={{.Node.Hostname}}";
+        };
+      };
     };
   };
   users.users.${variables.username} = {
