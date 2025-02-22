c839f6682e964476f0499e98e47eb2960cbfd681
EloToJaa <elotoja@protonmail.com>
2025-02-22 22:59:57 +0100

    Add ghidra-auto

--- modules/cybersec/scripts/default.nix
+++ modules/cybersec/scripts/default.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: let
-  # ghidra-auto = pkgs.writers.writePython3Bin "ghidra-auto" {
-  #   libraries = with pkgs.python313Packages; [
-  #     click
-  #   ];
-  # } (builtins.readFile ./scripts/ghidra-auto.py);
+  ghidra-auto = pkgs.writers.writePython3Bin "ghidra-auto" {
+    libraries = with pkgs.python313Packages; [
+      click
+    ];
+  } (builtins.readFile ./scripts/ghidra-auto.py);
 in {
   home.packages = [
-    # ghidra-auto
+    ghidra-auto
   ];
 }
