825266367bb6fc61ab5fd78d3ebfbdd9075b23b6
EloToJaa <elotoja@protonmail.com>
2025-02-28 21:56:31 +0100

    Change tab & split keymaps

--- modules/dev/nvim/nvim/lua/elotoja/core/keymaps.lua
+++ modules/dev/nvim/nvim/lua/elotoja/core/keymaps.lua
@@ -12,18 +12,18 @@ keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- incremen
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>s", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>ss", "<C-w>v", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
+keymap.set("n", "<leader>v", "<C-w>s", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>s", "<C-w>v", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>e", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
 keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>t", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
+keymap.set("n", "<leader>q", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>[", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<leader>]", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<leader>b", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
 
 for i = 1, 9 do
-	keymap.set("n", "<leader>t" .. i, ":tabnext " .. i .. "<CR>", { desc = "Go to tab " .. i })
+	keymap.set("n", "<leader>" .. i, ":tabnext " .. i .. "<CR>", { desc = "Go to tab " .. i })
 end
-keymap.set("n", "<leader>t0", ":tabnext 10<CR>", { desc = "Go to tab 10" })
+keymap.set("n", "<leader>0", ":tabnext 10<CR>", { desc = "Go to tab 10" })
