888f66df3784a30751af18e58c7d6a5adba927a3
EloToJaa <elotoja@protonmail.com>
2025-02-28 22:34:20 +0100

    Update diagnostics keymaps

--- modules/dev/nvim/nvim/lua/elotoja/plugins/trouble.lua
+++ modules/dev/nvim/nvim/lua/elotoja/plugins/trouble.lua
@@ -7,17 +7,17 @@ return {
 	cmd = "Trouble",
 	keys = {
 		{
-			"<leader>dw",
+			"dw",
 			"<cmd>Trouble diagnostics toggle<CR>",
 			desc = "Open trouble workspace diagnostics",
 		},
 		{
-			"<leader>dd",
+			"dd",
 			"<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
 			desc = "Open trouble document diagnostics",
 		},
-		{ "<leader>dq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
-		{ "<leader>dl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
-		{ "<leader>dt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
+		{ "dq", "<cmd>Trouble quickfix toggle<CR>", desc = "Open trouble quickfix list" },
+		{ "dl", "<cmd>Trouble loclist toggle<CR>", desc = "Open trouble location list" },
+		{ "dt", "<cmd>Trouble todo toggle<CR>", desc = "Open todos in trouble" },
 	},
 }
