0a1dc5cf25f39d99fabe59a310d5d48d3ebd1120
EloToJaa <elotoja@protonmail.com>
2025-03-02 11:13:16 +0100

    Search sessions add

--- modules/dev/nvim/nvim/lua/elotoja/plugins/auto-session.lua
+++ modules/dev/nvim/nvim/lua/elotoja/plugins/auto-session.lua
@@ -12,5 +12,6 @@ return {
 
 		keymap.set("n", "<leader>wr", "<cmd>SessionRestore<CR>", { desc = "Restore session for cwd" }) -- restore last workspace session for current directory
 		keymap.set("n", "<leader>ws", "<cmd>SessionSave<CR>", { desc = "Save session for auto session root dir" }) -- save workspace session for current working directory
+		keymap.set("n", "<leader>wf", "<cmd>SessionSearch<CR>", { desc = "Search for session" }) -- search for session
 	end,
 }
