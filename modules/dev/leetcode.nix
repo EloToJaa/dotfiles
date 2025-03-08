ee4b8d0ea5997b0fe8bed6c043fc8194ecc9dd7d
EloToJaa <elotoja@protonmail.com>
2025-03-08 10:12:52 +0100

    Add leetcode

---
+++ modules/dev/leetcode.nix
@@ -0,0 +1,21 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    leetcode-cli
+  ];
+
+  home.file.".leetcode/leetcode.toml".text = ''
+    [code]
+    editor = "nvim"
+    lang = "cpp"
+    comment_problem_desc = true
+    test = true
+    inject_before = ["#include<bits/stdc++.h>", "using namespace std;"]
+    inject_after = ["int main() {\n  Solution solution;\n\n}"]
+
+    [storage]
+    cache = 'Problems'
+    code = 'code'
+    root = '~/.leetcode'
+    scripts = 'scripts'
+  '';
+}
