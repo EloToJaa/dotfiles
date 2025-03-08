3b0d1634dc2abcdd17cfda76f9240a6299e14816
EloToJaa <elotoja@protonmail.com>
2025-03-08 11:57:38 +0100

    Fix path

--- modules/dev/leetcode.nix
+++ modules/dev/leetcode.nix
@@ -7,24 +7,27 @@
     leetcode-cli
   ];
 
-  sops.templates.".leetcode/leetcode.toml".content = ''
-    [code]
-    editor = "nvim"
-    lang = "cpp"
-    comment_leading = "//"
-    comment_problem_desc = true
-    test = true
-    inject_before = ["#include<bits/stdc++.h>", "using namespace std;"]
-    inject_after = ["int main() {\n  Solution solution;\n\n}"]
+  sops.templates."leetcode.toml" = {
+    content = ''
+      [code]
+      editor = "nvim"
+      lang = "cpp"
+      comment_leading = "//"
+      comment_problem_desc = true
+      test = true
+      inject_before = ["#include<bits/stdc++.h>", "using namespace std;"]
+      inject_after = ["int main() {\n  Solution solution;\n\n}"]
 
-    [cookies]
-    csrf = "${config.sops.placeholder."cookies/csrftoken"}"
-    session = "${config.sops.placeholder."cookies/session"}"
+      [cookies]
+      csrf = "${config.sops.placeholder."cookies/csrftoken"}"
+      session = "${config.sops.placeholder."cookies/session"}"
 
-    [storage]
-    cache = "Problems"
-    code = "code"
-    root = "~/.leetcode"
-    scripts = "scripts"
-  '';
+      [storage]
+      cache = "Problems"
+      code = "code"
+      root = "~/.leetcode"
+      scripts = "scripts"
+    '';
+    path = "${config.home.homeDirectory}/.leetcode/leetcode.toml";
+  };
 }
