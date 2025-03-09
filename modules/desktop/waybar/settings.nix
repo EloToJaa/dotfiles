c63ba8953fe53f50af94f647778ae54b8fe7d6a9
EloToJaa <elotoja@protonmail.com>
2025-03-09 12:08:05 +0100

    Remove roman literals for workspaces

--- modules/desktop/waybar/settings.nix
+++ modules/desktop/waybar/settings.nix
@@ -66,16 +66,6 @@ in {
       format = "{icon}";
       on-click = "activate";
       format-icons = {
-        "1" = "I";
-        "2" = "II";
-        "3" = "III";
-        "4" = "IV";
-        "5" = "V";
-        "6" = "VI";
-        "7" = "VII";
-        "8" = "VIII";
-        "9" = "IX";
-        "10" = "X";
         sort-by-number = true;
       };
       persistent-workspaces = {
