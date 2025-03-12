db9d411e5bfdc3ab84eba0fbc44715d70f12b2e1
EloToJaa <elotoja@protonmail.com>
2025-03-12 22:52:40 +0100

    Add ohmypost syntax

--- modules/home/ohmyposh/default.nix
+++ modules/home/ohmyposh/default.nix
@@ -2,16 +2,28 @@
   home.packages = with pkgs; [oh-my-posh];
 
   programs = {
-    zsh.initExtra = ''
-      eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
-    '';
+    zsh.initExtra =
+      /*
+      sh
+      */
+      ''
+        eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
+      '';
     nushell = {
-      extraEnv = ''
-        oh-my-posh init nu --config ~/.config/oh-my-posh/config.toml --print | save ~/.config/nushell/oh-my-posh.nu --force
-      '';
-      extraConfig = ''
-        source ~/.config/nushell/oh-my-posh.nu
-      '';
+      extraEnv =
+        /*
+        nu
+        */
+        ''
+          oh-my-posh init nu --config ~/.config/oh-my-posh/config.toml --print | save ~/.config/nushell/oh-my-posh.nu --force
+        '';
+      extraConfig =
+        /*
+        nu
+        */
+        ''
+          source ~/.config/nushell/oh-my-posh.nu
+        '';
     };
   };
 
