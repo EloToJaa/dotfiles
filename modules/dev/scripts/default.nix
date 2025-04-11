d755374de580a67afff1f29b4a84bbd0a8d9c5e0
EloToJaa <elotoja@protonmail.com>
2025-04-12 01:15:14 +0200

    Remove compress & extract from scripts config

--- modules/dev/scripts/default.nix
+++ modules/dev/scripts/default.nix
@@ -1,21 +1,11 @@
 {pkgs, ...}: let
   ascii = pkgs.writeScriptBin "ascii" (builtins.readFile ./scripts/ascii.sh);
-
-  compress = pkgs.writeScriptBin "compress" (builtins.readFile ./scripts/compress.sh);
-  extract = pkgs.writeScriptBin "extract" (builtins.readFile ./scripts/extract.sh);
-
   maxfetch = pkgs.writeScriptBin "maxfetch" (builtins.readFile ./scripts/maxfetch.sh);
-
   runbg = pkgs.writeShellScriptBin "runbg" (builtins.readFile ./scripts/runbg.sh);
 in {
   home.packages = [
     ascii
-
-    compress
-    extract
-
     maxfetch
-
     runbg
   ];
 }
