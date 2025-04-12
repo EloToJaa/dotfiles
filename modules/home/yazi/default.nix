d428f81cb47a3729add9d848e2b460a4575e42fa
EloToJaa <elotoja@protonmail.com>
2025-04-12 19:53:09 +0200

    Update yazi

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -18,37 +18,6 @@
     # yazi
   ];
 
-  programs.zsh.initExtra =
-    /*
-    sh
-    */
-    ''
-      function y() {
-      	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-      	yazi "$@" --cwd-file="$tmp"
-      	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-      		builtin cd -- "$cwd"
-      	fi
-      	rm -f -- "$tmp"
-      }
-    '';
-
-  programs.nushell.extraEnv =
-    /*
-    nu
-    */
-    ''
-      def --env y [...args] {
-       let tmp = (mktemp -t "yazi-cwd.XXXXXX")
-       yazi ...$args --cwd-file $tmp
-       let cwd = (open $tmp)
-       if $cwd != "" and $cwd != $env.PWD {
-        cd $cwd
-       }
-       rm -fp $tmp
-      }
-    '';
-
   imports = [
     inputs.nix-yazi-plugins.legacyPackages.x86_64-linux.homeManagerModules.default
   ];
@@ -99,11 +68,34 @@
     };
   };
 
-  # xdg.configFile = {
-  #   "yazi/yazi.toml".source = ./yazi.toml;
-  #   "yazi/keymap.toml".source = ./keymap.toml;
-  #   "yazi/theme.toml".source = ./theme.toml;
-  #   "yazi/Catppuccin-mocha.tmTheme".source = ./theme.tmTheme;
-  #   "yazi/init.lua".source = ./init.lua;
-  # };
+  programs.zsh.initExtra =
+    /*
+    sh
+    */
+    ''
+      function y() {
+      	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+      	yazi "$@" --cwd-file="$tmp"
+      	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+      		builtin cd -- "$cwd"
+      	fi
+      	rm -f -- "$tmp"
+      }
+    '';
+
+  programs.nushell.extraEnv =
+    /*
+    nu
+    */
+    ''
+      def --env y [...args] {
+       let tmp = (mktemp -t "yazi-cwd.XXXXXX")
+       yazi ...$args --cwd-file $tmp
+       let cwd = (open $tmp)
+       if $cwd != "" and $cwd != $env.PWD {
+        cd $cwd
+       }
+       rm -fp $tmp
+      }
+    '';
 }
