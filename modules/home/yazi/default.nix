074e7406eaaab82cfa1720aafca69e046447993e
EloToJaa <elotoja@protonmail.com>
2025-04-13 17:10:29 +0200

    Add diff plugin

--- modules/home/yazi/default.nix
+++ modules/home/yazi/default.nix
@@ -28,6 +28,7 @@
     "yazi/plugins/git.yazi".source = pkgs.callPackage ./plugins/git.nix {};
     "yazi/plugins/smart-filter.yazi".source = pkgs.callPackage ./plugins/smart-filter.nix {};
     "yazi/plugins/chmod.yazi".source = pkgs.callPackage ./plugins/chmod.nix {};
+    "yazi/plugins/diff.yazi".source = pkgs.callPackage ./plugins/diff.nix {};
     "yazi/plugins/copy-file-contents.yazi".source = pkgs.callPackage ./plugins/copy-file-contents.nix {};
     "yazi/plugins/exifaudio.yazi".source = pkgs.callPackage ./plugins/exifaudio.nix {};
   };
