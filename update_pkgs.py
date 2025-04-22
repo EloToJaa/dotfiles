36d4d2a065be3d5bc64f6cf239f9d6c48bf95224
EloToJaa <elotoja@protonmail.com>
2025-04-22 21:02:33 +0200

    Update python script

--- update_pkgs.py
+++ update_pkgs.py
@@ -6,7 +6,6 @@ import subprocess
 
 
 def run_command(command: str):
-    print(command)
     try:
         result = subprocess.run(command, capture_output=True, text=True, check=True, shell=True)
         return result.stdout
@@ -19,13 +18,11 @@ def get_hash_and_rev(owner: str, repo: str) -> tuple[str | None, str | None]:
     output = run_command(f"nurl https://github.com/{owner}/{repo} --json --fetcher fetchFromGitHub")
     if output is None:
         return None, None
-    # get json from nurl output
+
     json_data = json.loads(output)["args"]
     hash = json_data["hash"]
     rev = json_data["rev"]
 
-    print(f"Got hash: {hash}")
-    print(f"Got rev: {rev}")
     return hash, rev
 
 def get_owner_and_repo(file_path: str) -> tuple[str | None, str | None]:
@@ -41,15 +38,12 @@ def get_owner_and_repo(file_path: str) -> tuple[str | None, str | None]:
     owner = owner_match.group(1)
     repo = repo_match.group(1)
 
-    print(f"Found owner: {owner}")
-    print(f"Found repo: {repo}")
     return owner, repo
 
 def get_version() -> str:
     date_now = datetime.datetime.now()
     date_string = date_now.strftime("%Y-%m-%d")
     version = f"unstable-{date_string}"
-    print(f"Version: {version}")
     return version
 
 def write_to_file(file_path: str, hash: str, rev: str, version: str):
@@ -81,16 +75,15 @@ def process_file(file_path: str):
     write_to_file(file_path, hash, rev, version)
 
 def get_all_files(directory: str) -> list[str]:
-    files: list[str] = []
+    new_files: list[str] = []
     for root, _, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
-            extension = os.path.splitext(file_path)[1]
-            print(f"Checking {file_path} with extension {extension}")
+            extension = os.path.splitext(file)[1]
             if extension == ".nix":
-                files.append(file_path)
+                new_files.append(file_path)
             
-    return files
+    return new_files
 
 def process_directory(directory: str):
     files = get_all_files(directory)
