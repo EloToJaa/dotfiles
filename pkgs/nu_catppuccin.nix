26d50e77f1f894a8c9113ff65ed1682df751c230
EloToJaa <elotoja@protonmail.com>
2025-04-22 20:52:34 +0200

    Remove pname

--- pkgs/nu_catppuccin.nix
+++ pkgs/nu_catppuccin.nix
@@ -3,12 +3,11 @@
   lib,
 }:
 pkgs.stdenvNoCC.mkDerivation rec {
-  pname = "nushell";
   version = "0-unstable-2025-04-01";
 
   src = pkgs.fetchFromGitHub {
     owner = "catppuccin";
-    repo = pname;
+    repo = "nushell";
     rev = "82c31124b39294c722f5853cf94edc01ad5ddf34";
     hash = "sha256-O95OrdF9UA5xid1UlXzqrgZqw3fBpTChUDmyExmD2i4=";
   };
--- pkgs/nu_scripts.nix
+++ pkgs/nu_scripts.nix
@@ -3,12 +3,11 @@
   lib,
 }:
 pkgs.stdenvNoCC.mkDerivation rec {
-  pname = "nu_scripts";
   version = "0-unstable-2025-04-01";
 
   src = pkgs.fetchFromGitHub {
     owner = "nushell";
-    repo = pname;
+    repo = "nu_scripts";
     rev = "a19339cdaa94db45206d0656ccad57020b4830c9";
     hash = "sha256-l96wciWs4HVIMpbAWUr5fbcdKcGFOzkmBDGtad7/xd4=";
   };
