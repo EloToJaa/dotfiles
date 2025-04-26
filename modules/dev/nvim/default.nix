be32f9c14c41db6db00856e15bce6f92a1a80694
EloToJaa <elotoja@protonmail.com>
2025-04-26 14:10:50 +0200

    Remove bin from .dotnet

--- modules/dev/nvim/default.nix
+++ modules/dev/nvim/default.nix
@@ -67,7 +67,7 @@ in {
     EDITOR = "nvim";
     VISUAL = "nvim";
     MANPAGER = "nvim +Man!";
-    PATH = "${makeBinPath ["${homeDirectory}/.local/share/nvim/mason"]}:${makeBinPath ["${homeDirectory}/go"]}:${makeBinPath ["${homeDirectory}/.cargo"]}:${makeBinPath ["${homeDirectory}/.local"]}:${makeBinPath ["${homeDirectory}/.dotnet"]}:$PATH";
+    PATH = "${makeBinPath ["${homeDirectory}/.local/share/nvim/mason"]}:${makeBinPath ["${homeDirectory}/go"]}:${makeBinPath ["${homeDirectory}/.cargo"]}:${makeBinPath ["${homeDirectory}/.local"]}:${homeDirectory}/.dotnet:$PATH";
   };
 
   nix.nixPath = ["nixpkgs=${inputs.nixpkgs}"];
@@ -82,7 +82,7 @@ in {
     nushell = {
       shellAliases = shellAliases;
       extraEnv = ''
-        $env.Path = ($env.Path | prepend ["${makeBinPath ["${homeDirectory}/.local/share/nvim/mason"]}" "${makeBinPath ["${homeDirectory}/go"]}" "${makeBinPath ["${homeDirectory}/.cargo"]}" "${makeBinPath ["${homeDirectory}/.local"]}" "${makeBinPath ["${homeDirectory}/.dotnet"]}"]);
+        $env.Path = ($env.Path | prepend ["${makeBinPath ["${homeDirectory}/.local/share/nvim/mason"]}" "${makeBinPath ["${homeDirectory}/go"]}" "${makeBinPath ["${homeDirectory}/.cargo"]}" "${makeBinPath ["${homeDirectory}/.local"]}" "${homeDirectory}/.dotnet"]);
       '';
     };
   };
