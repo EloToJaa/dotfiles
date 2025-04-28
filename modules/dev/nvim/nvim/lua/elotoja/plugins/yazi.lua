0cf4161af84b6f57e5a7d11402de51fd73d3e6b5
EloToJaa <elotoja@protonmail.com>
2025-04-28 20:56:17 +0200

    Update yazi.nvim

--- modules/dev/nvim/nvim/lua/elotoja/plugins/yazi.lua
+++ modules/dev/nvim/nvim/lua/elotoja/plugins/yazi.lua
@@ -2,6 +2,9 @@
 return {
 	"mikavilpas/yazi.nvim",
 	event = "VeryLazy",
+	dependencies = {
+		"folke/snacks.nvim",
+	},
 	keys = {
 		-- 👇 in this section, choose your own keymappings!
 		{
@@ -23,7 +26,7 @@ return {
 			desc = "Resume the last yazi session",
 		},
 	},
-	---@type YaziConfig
+	---@type YaziConfig | {}
 	opts = {
 		-- if you want to open yazi instead of netrw, see below for more info
 		open_for_directories = true,
@@ -31,4 +34,10 @@ return {
 			show_help = "<f1>",
 		},
 	},
+	-- 👇 if you use `open_for_directories=true`, this is recommended
+	init = function()
+		-- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
+		-- vim.g.loaded_netrw = 1
+		vim.g.loaded_netrwPlugin = 1
+	end,
 }
