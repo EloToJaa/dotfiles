4a10cd4e69bbcf2e73ad9e50bcbae0d98114528f
EloToJaa <elotoja@protonmail.com>
2025-04-28 19:42:40 +0200

    Add firewall to server

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -12,6 +12,18 @@
   networking = {
     useDHCP = false;
     nameservers = ["192.168.0.31" "9.9.9.9" "149.112.112.112"];
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        22
+        53
+        80
+        443
+      ];
+      allowedUDPPorts = [
+        53
+      ];
+    };
     interfaces."enp1s0".ipv4.addresses = [
       {
         address = "192.168.0.32";
