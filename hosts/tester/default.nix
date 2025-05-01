6f3a4e51edf9ab4732057b4bfc10396a7869e6c3
EloToJaa <elotoja@protonmail.com>
2025-05-01 20:13:22 +0200

    Add tester

---
+++ hosts/tester/default.nix
@@ -0,0 +1,28 @@
+{variables, ...}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/base
+    ./../../modules/homelab
+  ];
+
+  home-manager.users.${variables.username}.imports = [
+    ./../../modules/home
+  ];
+
+  networking = {
+    useDHCP = false;
+    nameservers = variables.dns;
+    interfaces."enp1s0".ipv4.addresses = [
+      {
+        address = "192.168.0.30";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      address = "192.168.0.1";
+      interface = "enp1s0";
+    };
+  };
+
+  powerManagement.cpuFreqGovernor = "performance";
+}
