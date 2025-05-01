2648b96a996974208af7f01463228e5f8ab79367
EloToJaa <elotoja@protonmail.com>
2025-05-01 15:54:56 +0200

    Change path in nixosModule for sops

--- modules/base/sops.nix
+++ modules/base/sops.nix
@@ -1,6 +1,6 @@
 {
   inputs,
-  config,
+  variables,
   ...
 }: {
   imports = [
@@ -9,5 +9,5 @@
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
 
-  sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+  sops.age.keyFile = "/home/${variables.username}/.config/sops/age/keys.txt";
 }
