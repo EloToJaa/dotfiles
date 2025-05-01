d6704bbaf703eea608a0ee8734c94b47e3dd5a5c
EloToJaa <elotoja@protonmail.com>
2025-05-01 15:51:38 +0200

    Add sops to base

--- modules/home/sops.nix
+++ modules/home/sops.nix
@@ -1,13 +1,24 @@
 {
   inputs,
+  pkgs,
   config,
   ...
 }: {
   imports = [
-    inputs.sops-nix.nixosModules.sops
+    inputs.sops-nix.homeManagerModules.sops
   ];
   sops.defaultSopsFile = ../../secrets/secrets.yaml;
   sops.defaultSopsFormat = "yaml";
 
   sops.age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+
+  sops.secrets = {
+    "leetcode/csrftoken" = {};
+    "leetcode/session" = {};
+    "aoc/session" = {};
+  };
+
+  home.packages = with pkgs; [
+    sops
+  ];
 }
