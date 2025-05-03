39be50c26cda0eca77d127125c56ba8548f75ea5
EloToJaa <elotoja@protonmail.com>
2025-05-03 22:52:03 +0200

    Add config.xml to sonarr

--- modules/homelab/sonarr/default.nix
+++ modules/homelab/sonarr/default.nix
@@ -1,6 +1,7 @@
 {
   variables,
   lib,
+  config,
   ...
 }: let
   name = "sonarr";
@@ -41,11 +42,42 @@ in {
     group = "${group}";
   };
 
+  sops.secrets = {
+    "sonarr/apikey" = {
+      owner = name;
+    };
+    "sonarr/pgpassword" = {
+      owner = name;
+    };
+  };
   sops.templates = {
     "config.xml" = {
       content = ''
+        <Config>
+          <LogLevel>info</LogLevel>
+          <EnableSsl>False</EnableSsl>
+          <Port>${toString port}</Port>
+          <SslPort>9898</SslPort>
+          <UrlBase></UrlBase>
+          <BindAddress>*</BindAddress>
+          <ApiKey>${config.sops.placeholder."sonarr/apikey"}</ApiKey>
+          <AuthenticationMethod>Forms</AuthenticationMethod>
+          <LaunchBrowser>True</LaunchBrowser>
+          <Branch>main</Branch>
+          <InstanceName>Sonarr</InstanceName>
+          <AuthenticationRequired>Enabled</AuthenticationRequired>
+          <SslCertPath></SslCertPath>
+          <SslCertPassword></SslCertPassword>
+          <PostgresUser>${name}</PostgresUser>
+          <PostgresPassword>${config.sops.placeholder."sonarr/pgpassword"}</PostgresPassword>
+          <PostgresPort>5432</PostgresPort>
+          <PostgresHost>127.0.0.1</PostgresHost>
+          <AnalyticsEnabled>False</AnalyticsEnabled>
+          <Theme>auto</Theme>
+        </Config>
       '';
       path = "${homelab.dataDir}${name}/config.xml";
+      owner = name;
     };
   };
 }
