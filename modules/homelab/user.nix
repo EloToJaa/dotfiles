e1eca6cc67b698a4dc333cc264b1aff74e76dbcd
EloToJaa <elotoja@protonmail.com>
2025-05-03 15:05:26 +0200

    Add new groups for homelab

--- modules/homelab/user.nix
+++ modules/homelab/user.nix
@@ -5,11 +5,17 @@ in {
     extraGroups = [
       "${homelab.groups.main}"
       "${homelab.groups.media}"
+      "${homelab.groups.photos}"
+      "${homelab.groups.docs}"
+      "${homelab.groups.database}"
     ];
   };
 
   users.groups = {
     ${homelab.groups.main}.gid = 1100;
     ${homelab.groups.media}.gid = 1101;
+    ${homelab.groups.photos}.gid = 1102;
+    ${homelab.groups.docs}.gid = 1103;
+    ${homelab.groups.database}.gid = 1104;
   };
 }
