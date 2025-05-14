8c80f5090a9a4ed711a2e17f8cb9b55ec315d4f5
EloToJaa <elotoja@protonmail.com>
2025-05-14 22:18:39 +0200

    Update wireguard

--- modules/homelab/wireguard/default.nix
+++ modules/homelab/wireguard/default.nix
@@ -1,18 +1,44 @@
-{
-  variables,
-  config,
-  ...
-}: let
-  name = "wireguard-netns";
+{config, ...}: let
+  name = "wireguard";
+  privateIP = "10.73.69.202";
+  dnsIP = "10.64.0.1";
 in {
   imports = [
     ./service.nix
   ];
 
-  services.${name} = {
+  services."${name}-netns" = {
     enable = true;
     configFile = config.age.secrets.wireguardCredentials.path;
-    privateIP = "10.100.0.2";
-    dnsIP = "10.64.0.1";
+    privateIP = privateIP;
+    dnsIP = dnsIP;
+  };
+
+  sops.secrets = {
+    "${name}/privatekey" = {
+      owner = name;
+    };
+    "${name}/publickey" = {
+      owner = name;
+    };
+    "${name}/endpoint" = {
+      owner = name;
+    };
+  };
+  sops.templates = {
+    "config-${name}.xml" = {
+      content = ''
+        [Interface]
+        PrivateKey = ${config.sops.placeholder."${name}/privatekey"}
+        Address = ${privateIP}/32
+        DNS = ${dnsIP}
+
+        [Peer]
+        PublicKey = ${config.sops.placeholder."${name}/publickey"}
+        AllowedIPs = 0.0.0.0/0,::0/0
+        Endpoint = ${config.sops.placeholder."${name}/endpoint"}
+      '';
+      owner = name;
+    };
   };
 }
