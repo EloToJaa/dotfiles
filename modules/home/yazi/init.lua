93cc5d5bb9fd73725b8c372ad223e7ee3db93847
EloToJaa <elotoja@protonmail.com>
2025-05-17 03:30:29 +0200

    Update db for zoxide

--- modules/home/yazi/init.lua
+++ modules/home/yazi/init.lua
@@ -1,3 +1,7 @@
+require("zoxide"):setup({
+	update_db = true,
+})
+
 require("git"):setup()
 
 require("relative-motions"):setup({ show_numbers = "relative", show_motion = true, enter_mode = "first" })
