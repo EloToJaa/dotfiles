ef507de35f2a67cdb572b2e545f0d05e86b146a6
EloToJaa <elotoja@protonmail.com>
2025-06-01 13:21:19 +0200

    Add dnsmasq

---
+++ modules/homelab/dnsmasq/default.nix
@@ -0,0 +1,18 @@
+{variables, ...}: let
+  name = "dnsmasq";
+  homelab = variables.homelab;
+in {
+  services.${name} = {
+    enable = true;
+    alwaysKeepRunning = true;
+    resolveLocalQueries = true;
+    settings = {
+      server = [
+        "9.9.9.9"
+        "149.112.112.112"
+        "2620:fe::fe"
+        "2620:fe::9"
+      ];
+    };
+  };
+}
