adc70ab112990fc76907284f2ad4d89f976485e9
EloToJaa <elotoja@protonmail.com>
2025-06-10 22:42:30 +0200

    Add spicetify

--- flake.nix
+++ flake.nix
@@ -18,6 +18,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    spicetify-nix = {
+      url = "github:gerg-l/spicetify-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     sops-nix.url = "github:Mic92/sops-nix";
     deploy-rs.url = "github:serokell/deploy-rs";
     yazi.url = "github:sxyazi/yazi/b65a88075a824e4304dca5428ba05de1404fa635";
--- modules/desktop/spotify.nix
+++ modules/desktop/spotify.nix
@@ -1,5 +1,26 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  inputs,
+  variables,
+  ...
+}: let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+in {
   home.packages = with pkgs; [
     spotube
   ];
+
+  imports = [inputs.spicetify-nix.homeManagerModules.default];
+
+  programs.spicetify = {
+    enable = true;
+
+    enabledExtensions = with spicePkgs.extensions; [
+      adblock
+      shuffle # shuffle+ (special characters are sanitized out of extension names)
+    ];
+
+    theme = spicePkgs.themes.catppuccin;
+    colorScheme = variables.catppuccin.flavor;
+  };
 }
