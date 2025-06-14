bafdf009bdece267324b486e3d2efccca1ac517e
EloToJaa <elotoja@protonmail.com>
2025-06-14 18:15:52 +0200

    Update telescope

--- modules/dev/nvim/plugins/telescope.nix
+++ modules/dev/nvim/plugins/telescope.nix
@@ -5,40 +5,41 @@
 }: let
   pushDownForce = attr: lib.mapAttrs (_: lib.mkForce) attr;
 in {
-  programs.nvf.settings.vim.telescope = {
-    enable = true;
+  programs.nvf.settings.vim = {
+    telescope = {
+      enable = true;
 
-    mappings = {
-      findFiles = "<C-p>";
-      liveGrep = "<leader>fs";
-      treesitter = "<leader>ff";
+      mappings = {
+        findFiles = "<C-p>";
+        liveGrep = "<leader>fs";
+        treesitter = "<leader>ff";
 
-      gitCommits = "<leader>fgc";
-      gitBufferCommits = "<leader>fgf";
-      gitBranches = "<leader>fgb";
-      gitStatus = "<leader>fgs";
-      gitStash = "<leader>fgx";
-    };
+        gitCommits = "<leader>fgc";
+        gitBufferCommits = "<leader>fgf";
+        gitBranches = "<leader>fgb";
+        gitStatus = "<leader>fgs";
+        gitStash = "<leader>fgx";
+      };
 
-    extensions = [
-      {
-        name = "fzf";
-        packages = [pkgs.vimPlugins.telescope-fzf-native-nvim];
-        setup = {
-          fzf = {
-            fuzzy = true;
-            override_generic_sorter = true;
-            override_file_sorter = true;
-            case_mode = "smart_case";
+      extensions = [
+        {
+          name = "fzf";
+          packages = [pkgs.vimPlugins.telescope-fzf-native-nvim];
+          setup = {
+            fzf = {
+              fuzzy = true;
+              override_generic_sorter = true;
+              override_file_sorter = true;
+              case_mode = "smart_case";
+            };
           };
-        };
-      }
-    ];
-  };
-
-  binds.whichKey.register = pushDownForce {
-    "<leader>fv" = "Yazi";
-    "<leader>fg" = "Telescope Git";
-    "<leader>fgc" = "Commits";
+        }
+      ];
+    };
+    binds.whichKey.register = pushDownForce {
+      "<leader>fv" = "Yazi";
+      "<leader>fg" = "Telescope Git";
+      "<leader>fgc" = "Commits";
+    };
   };
 }
