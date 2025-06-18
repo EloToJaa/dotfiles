00fb444aa1c7fda73f93454ccb84071580234890
EloToJaa <elotoja@protonmail.com>
2025-06-18 22:15:32 +0200

    Remove web-devicons

--- modules/dev/nvim/plugins/snacks/default.nix
+++ modules/dev/nvim/plugins/snacks/default.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs.vimPlugins; [
     snacks-nvim
-    nvim-web-devicons
   ];
   xdg.configFile."nvim/lua/elotoja/plugins/snacks.lua".source = ./snacks.lua;
 }
