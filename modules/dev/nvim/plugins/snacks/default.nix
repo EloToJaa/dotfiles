e20ad0ff2b1a0899f6e9887277406a9e20374252
EloToJaa <elotoja@protonmail.com>
2025-06-18 23:25:21 +0200

    Revert "Disable mini icons"
    This reverts commit 6f66bb8f4f819dd431ca91855961f9ee8ad1b7c5.

--- modules/dev/nvim/plugins/snacks/default.nix
+++ modules/dev/nvim/plugins/snacks/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs.vimPlugins; [
     snacks-nvim
+    mini-icons
     nvim-web-devicons
   ];
   xdg.configFile."nvim/lua/elotoja/plugins/snacks.lua".source = ./snacks.lua;
