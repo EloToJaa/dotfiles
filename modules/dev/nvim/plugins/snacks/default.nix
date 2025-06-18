6f66bb8f4f819dd431ca91855961f9ee8ad1b7c5
EloToJaa <elotoja@protonmail.com>
2025-06-18 22:13:52 +0200

    Disable mini icons

--- modules/dev/nvim/plugins/snacks/default.nix
+++ modules/dev/nvim/plugins/snacks/default.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   home.packages = with pkgs.vimPlugins; [
     snacks-nvim
-    mini-icons
     nvim-web-devicons
   ];
   xdg.configFile."nvim/lua/elotoja/plugins/snacks.lua".source = ./snacks.lua;
