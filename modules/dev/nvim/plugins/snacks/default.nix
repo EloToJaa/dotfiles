b51b53b619a2790d279de2e2496fb62f8ed27e86
EloToJaa <elotoja@protonmail.com>
2025-06-18 23:25:21 +0200

    Revert "Remove web-devicons"
    This reverts commit 00fb444aa1c7fda73f93454ccb84071580234890.

--- modules/dev/nvim/plugins/snacks/default.nix
+++ modules/dev/nvim/plugins/snacks/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs.vimPlugins; [
     snacks-nvim
+    nvim-web-devicons
   ];
   xdg.configFile."nvim/lua/elotoja/plugins/snacks.lua".source = ./snacks.lua;
 }
