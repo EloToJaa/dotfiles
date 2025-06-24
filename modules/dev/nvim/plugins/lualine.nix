08fd1822227ba0d06a0ad38d5fb0f327ab46e986
EloToJaa <elotoja@protonmail.com>
2025-06-24 17:45:37 +0200

    Fix syntax

--- modules/dev/nvim/plugins/lualine.nix
+++ modules/dev/nvim/plugins/lualine.nix
@@ -3,8 +3,8 @@
     enable = true;
     settings = {
       options = {
-        section_separators = { left = ""; right = "" };
-				component_separators = { left = ""; right = "" };
+        section_separators = { left = ""; right = ""; };
+				component_separators = { left = ""; right = ""; };
       };
       sections = {
         lualine_a = [ "mode" ];
