7f4f2835ee39ba7854ec8e993fb02d2145f53c68
EloToJaa <elotoja@protonmail.com>
2025-06-24 18:17:20 +0200

    Add lint

--- modules/dev/nvim/languages/nix.nix
+++ modules/dev/nvim/languages/nix.nix
@@ -1,5 +1,19 @@
 {
-  programs.nixvim.lsp.servers.nixd = {
-    enable = true;
+  home.packages = with pkgs; [
+    alejandra
+    deadnix
+    statix
+  ];
+
+  programs.nixvim = {
+    lsp.servers.nixd = {
+      enable = true;
+    };
+    plugins.lint.lintersByFt = {
+      nix = ["deadnix" "statix"];
+    };
+    plugins.conform-nvim.settings.formatters_by_ft = {
+      nix = ["alejandra"];
+    };
   };
 }
