99b4c03c3a7c75591075277eb94d8b5a20f7c318
EloToJaa <elotoja@protonmail.com>
2025-06-24 17:07:50 +0200

    Add yazi plugin

--- modules/dev/nvim/plugins/default.nix
+++ modules/dev/nvim/plugins/default.nix
@@ -4,5 +4,6 @@
     ./supermaven.nix
     ./telescope.nix
     ./treesitter.nix
+    ./yazi.nix
   ];
 }
---
+++ modules/dev/nvim/plugins/yazi.nix
@@ -0,0 +1,9 @@
+{
+  programs.nixvim.plugins.yazi = {
+    enable = true;
+    settings = {
+      enable_mouse_support = true;
+      open_for_directories = true;
+    };
+  };
+}
