8ac95640353716b4132f2fb4e44e2c24ccc599d4
EloToJaa <elotoja@protonmail.com>
2025-06-25 05:57:08 +0200

    Update cmp to use supermaven

--- modules/dev/nvim/plugins/cmp.nix
+++ modules/dev/nvim/plugins/cmp.nix
@@ -1,34 +1,53 @@
 {
-  programs.nixvim.plugins.blink-cmp = {
-    enable = true;
-    setupLspCapabilities = true;
+  programs.nixvim.plugins = {
+    blink-cmp = {
+      enable = true;
+      setupLspCapabilities = true;
 
-    settings = {
-      keymap.preset = "super-tab";
-      signature.enabled = true;
-      appearance = {
-        use_nvim_cmp_as_default = true;
-        nerd_font_variant = "mono";
-      };
-      completion = {
-        accept = {
-          auto_brackets = {
-            enabled = true;
-            semantic_token_resolution.enabled = false;
+      settings = {
+        keymap.preset = "super-tab";
+        signature.enabled = true;
+        # windows.autocomplete.select = "auto_insert";
+        appearance = {
+          use_nvim_cmp_as_default = true;
+          nerd_font_variant = "mono";
+        };
+        completion = {
+          accept = {
+            auto_brackets = {
+              enabled = true;
+              semantic_token_resolution.enabled = false;
+            };
+          };
+          documentation.auto_show = true;
+        };
+        sources = {
+          # providers = {
+          #   buffer = {
+          #     score_offset = -7;
+          #   };
+          #   lsp = {
+          #     fallbacks = [];
+          #   };
+          # };
+          completion.enabled_providers = {
+            __unkeyed-1 = "supermaven";
+          };
+          providers = {
+            supermaven = {
+              name = "supermaven";
+              module = "blink.compat.source";
+              score_offset = 3;
+            };
           };
         };
-        documentation.auto_show = true;
       };
-      # sources = {
-      #   providers = {
-      #     buffer = {
-      #       score_offset = -7;
-      #     };
-      #     lsp = {
-      #       fallbacks = [];
-      #     };
-      #   };
-      # };
+    };
+    blink-compat = {
+      enable = true;
+      settings = {
+        impersonate_nvim_cmp = true;
+      };
     };
   };
 }
--- modules/dev/nvim/plugins/supermaven.nix
+++ modules/dev/nvim/plugins/supermaven.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       keymaps = {
-        accept_suggestion = "<C-Tab>";
+        accept_suggestion = null; #"<C-Tab>";
         clear_suggestion = "<C-]>";
         accept_word = "<C-j>";
       };
@@ -12,7 +12,7 @@
         cterm = 244;
       };
       log_level = "info";
-      disable_inline_completion = false;
+      disable_inline_completion = true;
       disable_keymaps = false;
       condition = config.lib.nixvim.mkRaw ''
         function() return false end
