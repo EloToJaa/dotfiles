beffc4f3cf586d7e24057abe501f6f20364a553d
EloToJaa <elotoja@protonmail.com>
2025-06-25 23:24:21 +0200

    Format files

--- modules/dev/nvim/plugins/lualine.nix
+++ modules/dev/nvim/plugins/lualine.nix
@@ -3,21 +3,33 @@
     enable = true;
     settings = {
       options = {
-        section_separators = { left = ""; right = ""; };
-				component_separators = { left = ""; right = ""; };
+        section_separators = {
+          left = "";
+          right = "";
+        };
+        component_separators = {
+          left = "";
+          right = "";
+        };
       };
       sections = {
         lualine_a = [
           {
             __unkeyed-1 = "mode";
-            separator = { left = ""; right = ""; };
+            separator = {
+              left = "";
+              right = "";
+            };
           }
         ];
-        lualine_x = [ "encoding" "fileformat" "filetype" ];
+        lualine_x = ["encoding" "fileformat" "filetype"];
         lualine_z = [
           {
             __unkeyed-1 = "location";
-            separator = { left = ""; right = ""; };
+            separator = {
+              left = "";
+              right = "";
+            };
           }
         ];
       };
--- modules/dev/nvim/plugins/yazi.nix
+++ modules/dev/nvim/plugins/yazi.nix
@@ -1,17 +1,19 @@
 {
   programs.nixvim.plugins = {
     yazi = {
-    enable = true;
-    settings = {
-      enable_mouse_support = true;
-      open_for_directories = true;
+      enable = true;
+      settings = {
+        enable_mouse_support = true;
+        open_for_directories = true;
+      };
     };
-  };
-    lz-n.keymaps = [{
-      action = "<cmd>Yazi<CR>";
-      key = "<leader>fv";
-      options.desc = "Open Yazi";
-      plugin = "yazi.nvim";
-      }];
+    lz-n.keymaps = [
+      {
+        action = "<cmd>Yazi<CR>";
+        key = "<leader>fv";
+        options.desc = "Open Yazi";
+        plugin = "yazi.nvim";
+      }
+    ];
   };
 }
