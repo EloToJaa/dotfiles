a555b1f815149fe95166e6c53bf726520c53f6f2
EloToJaa <elotoja@protonmail.com>
2025-06-25 21:54:37 +0200

    Remove unused cmp config

--- modules/dev/nvim/plugins/cmp.nix
+++ modules/dev/nvim/plugins/cmp.nix
@@ -5,40 +5,6 @@
       setupLspCapabilities = true;
 
       settings = {
-        # keymap.preset = "super-tab";
-        # signature.enabled = true;
-        # windows.autocomplete.select = "auto_insert";
-        # appearance = {
-        #   use_nvim_cmp_as_default = true;
-        #   nerd_font_variant = "mono";
-        # };
-        # completion = {
-        #   accept = {
-        #     auto_brackets = {
-        #       enabled = true;
-        #       semantic_token_resolution.enabled = false;
-        #     };
-        #   };
-        #   documentation.auto_show = true;
-        # };
-        # sources = {
-        #   default = [
-        #     "supermaven"
-        #   ];
-        #   providers = {
-        #     supermaven = {
-        #       name = "supermaven";
-        #       module = "blink.compat.source";
-        #       score_offset = 3;
-        #     };
-        #     buffer = {
-        #       score_offset = -7;
-        #     };
-        #     lsp = {
-        #       fallbacks = [];
-        #     };
-        #   };
-        # };
         sources = {
           default = ["lsp" "path" "snippets" "buffer" "supermaven"];
           providers = {
