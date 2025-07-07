c302ae0b9b39aa4240bd3ecb2d28867d67819f79
EloToJaa <elotoja@protonmail.com>
2025-07-07 19:34:46 +0200

    Add lsp

--- modules/dev/nvim/plugins/lsp.nix
+++ modules/dev/nvim/plugins/lsp.nix
@@ -36,6 +36,30 @@ in {
         key = "gD";
         action = mkRaw "vim.lsp.buf.declaration";
       }
+      {
+        mode = "n";
+        key = "<leader>rn";
+        action = mkRaw "vim.lsp.buf.rename";
+        options.desc = "Rename";
+      }
+      {
+        mode = "n";
+        key = "<leader>df";
+        action = "<cmd>Telescope diagnostics bufnr=0<CR>";
+        options.desc = "Show buffer diagnostics";
+      }
+      {
+        mode = "n";
+        key = "<leader>ds";
+        action = mkRaw "vim.diagnostic.open_float";
+        options.desc = "Show line diagnostics";
+      }
+      {
+        mode = "n";
+        key = "<leader>rs";
+        action = "<cmd>LspRestart<CR>";
+        options.desc = "Restart LSP";
+      }
     ];
     extraConfigLuaPost = ''
       local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
