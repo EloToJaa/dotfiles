ff66bb5edddf21561ef346d86a2d666429edfdfb
EloToJaa <elotoja@protonmail.com>
2025-07-19 17:08:08 +0200

    Change oh-my-posh location

--- modules/home/ohmyposh/default.nix
+++ modules/home/ohmyposh/default.nix
@@ -7,7 +7,7 @@
       sh
       */
       ''
-        eval "$(oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
+        eval "$(${pkgs.oh-my-posh} init zsh --config ~/.config/oh-my-posh/config.toml)"
       '';
     nushell = {
       extraEnv =
@@ -15,7 +15,7 @@
         nu
         */
         ''
-          oh-my-posh init nu --config ~/.config/oh-my-posh/config.toml --print | save ~/.config/nushell/oh-my-posh.nu --force
+          ${pkgs.oh-my-posh} init nu --config ~/.config/oh-my-posh/config.toml --print | save ~/.config/nushell/oh-my-posh.nu --force
         '';
       extraConfig =
         /*
