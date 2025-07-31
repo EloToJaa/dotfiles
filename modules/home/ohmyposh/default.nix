a545d322afd96d340260f69b3fbcefa0a5fdb167
EloToJaa <elotoja@protonmail.com>
2025-07-31 08:08:34 +0200

    Update nushell

--- modules/home/ohmyposh/default.nix
+++ modules/home/ohmyposh/default.nix
@@ -15,7 +15,7 @@
         nu
         */
         ''
-          ${pkgs.oh-my-posh}/bin/oh-my-posh init nu --config ~/.config/oh-my-posh/config.toml --print | save ~/.config/nushell/oh-my-posh.nu --force
+          ${pkgs.oh-my-posh}/bin/oh-my-posh init nu --config ~/.config/oh-my-posh/config.toml
         '';
       extraConfig =
         /*
