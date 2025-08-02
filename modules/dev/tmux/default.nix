be1a15b1fcff2903338c423870c673fba1a963e8
EloToJaa <elotoja@protonmail.com>
2025-08-02 13:57:41 +0200

    Add new options to tmux

--- modules/dev/tmux/default.nix
+++ modules/dev/tmux/default.nix
@@ -5,5 +5,9 @@
     terminal = "tmux-256color";
     historyLimit = 10000;
     clock24 = true;
+    baseIndex = 1;
+    keyMode = "vi";
+    escapeTime = 1000;
+    secureSocket = true; # Check if has to be false for tmux-resurrect
   };
 }
