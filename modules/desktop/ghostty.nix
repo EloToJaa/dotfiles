1309bdb692b33f0663c72f0e2bd83e60682a5b6c
EloToJaa <elotoja@protonmail.com>
2025-08-02 13:31:47 +0200

    Update ghostty config
    Test 1
    
    Test 2

--- modules/desktop/ghostty.nix
+++ modules/desktop/ghostty.nix
@@ -8,9 +8,58 @@
     installVimSyntax = true;
 
     settings = {
-      theme = "catppuccin-mocha";
+      font-family = [
+        "JetBrainsMono Nerd Font"
+        "Maple Mono"
+        "DejaVu Sans"
+      ];
       font-size = 12.4;
-      font-family = "JetBrainsMono Nerd Font";
+      font-feature = [
+        "calt"
+        "ss03"
+      ];
+
+      bold-is-bright = true;
+      selection-invert-fg-bg = true;
+
+      theme = "catppuccin-mocha";
+      background-opacity = 0.9;
+      background-blur = 10;
+
+      cursor-style = "bar";
+      cursor-style-blink = false;
+      adjust-cursor-thickness = 1;
+
+      resize-overlay = "never";
+      copy-on-select = false;
+      confirm-close-surface = false;
+      mouse-hide-while-typing = true;
+
+      window-theme = "ghostty";
+      window-padding-x = 4;
+      window-padding-y = 0;
+      window-padding-balance = true;
+      window-padding-color = "background";
+      window-inherit-working-directory = true;
+      window-inherit-font-size = true;
+      window-decoration = false;
+
+      gtk-titlebar = false;
+      gtk-single-instance = false;
+      gtk-tabs-location = "bottom";
+      gtk-wide-tabs = false;
+
+      auto-update = "off";
+      term = "ghostty";
+      clipboard-paste-protection = false;
+
+      keybind = [
+        "shift+end=unbind"
+        "shift+home=unbind"
+        "ctrl+shift+left=unbind"
+        "ctrl+shift+right=unbind"
+        "shift+enter=text:\n"
+      ];
     };
   };
 }
