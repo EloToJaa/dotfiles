7cb3e3e3e93f20812bdb01ef6e2dcf93b90edc91
EloToJaa <elotoja@protonmail.com>
2025-08-23 11:54:42 +0200

    Disable oh-my-posh

--- modules/home/ohmyposh/default.nix
+++ modules/home/ohmyposh/default.nix
@@ -1,11 +1,11 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [oh-my-posh];
 
-  programs = {
-    zsh.initContent = ''
-      eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
-    '';
-  };
+  # programs = {
+  #   zsh.initContent = ''
+  #     eval "$(${pkgs.oh-my-posh}/bin/oh-my-posh init zsh --config ~/.config/oh-my-posh/config.toml)"
+  #   '';
+  # };
 
   xdg.configFile."oh-my-posh/config.toml".source = ./config.toml;
 }
