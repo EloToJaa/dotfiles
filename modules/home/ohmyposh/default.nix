0b0c84f340e6e8d6e58ea4289a2643ba4e9edcbb
EloToJaa <elotoja@protonmail.com>
2025-08-23 12:38:54 +0200

    Test oh-my-posh

--- modules/home/ohmyposh/default.nix
+++ modules/home/ohmyposh/default.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [oh-my-posh];
+  # home.packages = with pkgs; [oh-my-posh];
 
   # programs = {
   #   zsh.initContent = ''
@@ -7,5 +7,25 @@
   #   '';
   # };
 
-  xdg.configFile."oh-my-posh/config.toml".source = ./config.toml;
+  # xdg.configFile."oh-my-posh/config.toml".source = ./config.toml;
+
+  # https://github.com/NovaViper/NixConfig/blob/51a5e1c261edb1deffaba6173a44de8d1002bee7/features/home/cli/prompt/oh-my-posh/posh.nix
+  programs.oh-my-posh = {
+    enable = true;
+    package = pkgs.oh-my-posh;
+    settings = {
+      "$schema" = "https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/schema.json";
+      version = 2;
+      final_space = true;
+      disable_notice = true;
+      transient_prompt = {
+        background = "transparent";
+        foreground_templates = [
+          "{{if gt .Code 0}}1{{end}}"
+          "{{if eq .Code 0}}10{{end}}"
+        ];
+        template = "{{.Var.PromptChar}} ";
+      };
+    };
+  };
 }
