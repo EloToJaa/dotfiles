87efa4bc2f688dcb28cb5f96b4974a0b98682816
EloToJaa <elotoja@protonmail.com>
2025-09-04 20:43:10 +0200

    Add atuin

---
+++ modules/homelab/atuin/default.nix
@@ -0,0 +1,52 @@
+{
+  variables,
+  pkgs,
+  config,
+  ...
+}: let
+  inherit (variables) homelab;
+  name = "atuin";
+  domainName = "atuin";
+  port = 8888;
+in {
+  services.${name} = {
+    enable = true;
+    package = pkgs.unstable.atuin;
+    database = {
+      uri = null;
+      createLocally = false;
+    };
+  };
+  systemd.services.${name}.serviceConfig = {
+    EnvironmentFile = config.sops.templates."${name}.env".path;
+  };
+
+  services.caddy.virtualHosts."${domainName}.${homelab.baseDomain}" = {
+    useACMEHost = homelab.baseDomain;
+    extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
+  };
+
+  services.postgresql.ensureUsers = [
+    {
+      inherit name;
+      ensureDBOwnership = false;
+    }
+  ];
+  services.postgresql.ensureDatabases = [
+    name
+  ];
+  services.postgresqlBackup.databases = [
+    name
+  ];
+
+  sops.secrets = {
+    "${name}/pgpassword" = {
+      owner = name;
+    };
+  };
+  sops.templates."${name}.env".content = ''
+    ATUIN_DB_URI=postgresql://${name}:${config.sops.placeholder."${name}/pgpassword"}@127.0.0.1:5432/${name}
+  '';
+}
