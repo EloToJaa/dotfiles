85aed851757094993a8a5f23c0bc386a7b89247c
EloToJaa <elotoja@protonmail.com>
2025-09-08 20:54:37 +0200

    Set systemd to 257.3

--- flake.nix
+++ flake.nix
@@ -6,7 +6,7 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     nur.url = "github:nix-community/NUR";
 
-    systemd-nixpkgs.url = "github:nixos/nixpkgs/372d9eeeafa5b15913201e2b92e8e539ac7c64d1";
+    systemd-nixpkgs.url = "github:nixos/nixpkgs/ed30f8aba41605e3ab46421e3dcb4510ec560ff8";
 
     hyprland = {
       url = "github:hyprwm/Hyprland/main?submodules=true";
