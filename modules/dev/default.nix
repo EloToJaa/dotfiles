fdd1836deb368e7692e0fb7426c462843f2744cd
EloToJaa <elotoja@protonmail.com>
2025-09-19 09:40:30 +0200

    Remove aliases from dev

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -3,7 +3,6 @@
     ./nvim
     ./scripts # personal scripts
     ./tmux
-    ./aliases.nix # shell aliases
     ./coding.nix # leetcode cli
     ./packages.nix # other packages
     ./variables.nix # environment variables
