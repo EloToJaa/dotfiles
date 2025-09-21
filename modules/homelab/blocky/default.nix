4eca44e4a36989e8e77f5527a6120a46b551f0df
EloToJaa <elotoja@protonmail.com>
2025-09-21 13:15:33 +0200

    Test dns

--- modules/homelab/blocky/default.nix
+++ modules/homelab/blocky/default.nix
@@ -18,21 +18,19 @@ in {
       # For initially solving DoH/DoT Requests when no system Resolver is available.
       bootstrapDns = [
         {
-          upstream = "https://one.one.one.one/dns-query";
-          ips = ["1.1.1.1" "1.0.0.1"];
-        }
-        {
           upstream = "https://dns.quad9.net/dns-query";
           ips = ["9.9.9.9" "149.112.112.112"];
+        }
+        {
+          upstream = "https://one.one.one.one/dns-query";
+          ips = ["1.1.1.1" "1.0.0.1"];
         }
       ];
       #Enable Blocking of certain domains.
       blocking = {
         denylists = {
-          #Adblocking
           ads = ["https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts"];
         };
-        #Configure what block categories are used
         clientGroupsBlock = {
           default = ["ads"];
         };
@@ -44,7 +42,7 @@ in {
       };
       customDNS = {
         customTTL = "1h";
-        mapping.${baseDomain} = "192.168.0.32";
+        mapping."local.elotoja.com" = "192.168.0.32";
       };
     };
   };
