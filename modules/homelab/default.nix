715c6e02063b5ff15385ab085e28291251886396
EloToJaa <elotoja@protonmail.com>
2025-10-02 02:43:32 +0200

    Add nextcloud to imports

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -1,92 +1,4 @@
 {
-  lib,
-  config,
-  ...
-}: let
-  cfg = config.homelab;
-in {
-  # options.homelab = {
-  #   enable = lib.mkEnableOption "The homelab services and configuration variables";
-  #   timeZone = lib.mkOption {
-  #     default = "Europe/Warsaw";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Time zone to be used for the homelab services
-  #     '';
-  #   };
-  #   mainDomain = lib.mkOption {
-  #     default = "";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Base domain name to be used to access the homelab services via Caddy reverse proxy
-  #     '';
-  #   };
-  #   baseDomain = lib.mkOption {
-  #     default = "";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Base domain name to be used to access the homelab services via Caddy reverse proxy
-  #     '';
-  #   };
-  #   dataDir = lib.mkOption {
-  #     default = "/opt/";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Base directory to be used for the homelab services data
-  #     '';
-  #   };
-  #   varDataDir = lib.mkOption {
-  #     default = "/var/lib/";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Base directory to be used for the homelab services data
-  #     '';
-  #   };
-  #   logDir = lib.mkOption {
-  #     default = "/var/log/";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Base directory to be used for the homelab services logs
-  #     '';
-  #   };
-  #   defaultUMask = lib.mkOption {
-  #     default = "027";
-  #     type = lib.types.str;
-  #     description = ''
-  #       Default umask to be used for the homelab services
-  #     '';
-  #   };
-  #   groups.main = lib.mkOption {
-  #     type = lib.types.str;
-  #     default = "homelab";
-  #     description = "Main homelab group";
-  #   };
-  #   groups.media = lib.mkOption {
-  #     type = lib.types.str;
-  #     default = "media";
-  #     description = "Media group";
-  #   };
-  #   groups.photos = lib.mkOption {
-  #     type = lib.types.str;
-  #     default = "photos";
-  #     description = "Photos group";
-  #   };
-  #   groups.docs = lib.mkOption {
-  #     type = lib.types.str;
-  #     default = "documents";
-  #     description = "Documents group";
-  #   };
-  #   groups.database = lib.mkOption {
-  #     type = lib.types.str;
-  #     default = "database";
-  #     description = "Database group";
-  #   };
-  #   groups.backups = lib.mkOption {
-  #     type = lib.types.str;
-  #     default = "backups";
-  #     description = "Backups group";
-  #   };
-  # };
   imports = [
     ./atuin
     ./backup
@@ -100,6 +12,7 @@ in {
     ./jellyseerr
     ./karakeep
     # ./loki
+    ./nextcloud
     ./paperless
     ./postgres
     # ./prometheus
--- modules/homelab/nextcloud/default.nix
+++ modules/homelab/nextcloud/default.nix
@@ -11,18 +11,41 @@
   group = variables.homelab.groups.docs;
   dataDir = "${homelab.varDataDir}${name}";
   domain = "${domainName}.${homelab.baseDomain}";
+  port = 3004;
+  occ = "${config.services.nextcloud.occ}/bin/nextcloud-occ";
 in {
-  services.nextcloud = {
+  shb.nextcloud = {
     enable = true;
     package = pkgs.nextcloud;
+
+    # Data
     home = dataDir;
     datadir = "/mnt/Data/nextcloud";
     database.createLocally = false;
+
+    # Apps
     autoUpdateApps.enable = false;
     appstoreEnable = true;
-    enableImagemagick = true;
+    extraAppsEnable = true;
+    extraApps = {
+      inherit (pkgs.nextcloud31Packages.apps) mail calendar contacts;
+    };
+
+    # Caching
+    # Enable caching using redis https://nixos.wiki/wiki/Nextcloud#Caching.
     configureRedis = true;
+    # https://docs.nextcloud.com/server/26/admin_manual/configuration_server/caching_configuration.html
     caching.redis = true;
+    caching.apcu = false;
+
+    # HTTP
+    https = false;
+    hostName = domain;
+    nginx.hstsMaxAge = 31536000;
+    webfinger = true;
+
+    # Settings
+    enableImagemagick = true;
     config = {
       adminuser = variables.username;
       adminpassFile = config.sops.secrets."${name}/adminpassword".path;
@@ -33,11 +56,23 @@ in {
       dbpassFile = config.sops.secrets."${name}/pgpassword";
     };
     settings = {
-    };
-    extraApps = {
-      inherit (pkgs.nextcloud31Packages.apps) mail calendar contacts;
+      default_phone_region = "PL";
+      overwrite.cli.url = "https://${domain}";
+      overwritehost = "${domain}:${port}";
+      trusted_domains = [domain];
+      trusted_proxies = "127.0.0.1";
+      overwriteprotocol = "https";
+      overwritecondaddr = "";
+      dbpersistent = "true";
+      chunkSize = "5120MB";
     };
   };
+
+  # This is needed to be able to run the cron job before opening the app for the first time.
+  # Otherwise the cron job fails while searching for this directory.
+  systemd.services.nextcloud-setup.script = ''
+    mkdir -p ${dataDir}/data/appdata_$(${occ} config:system:get instanceid)/theming/global
+  '';
 
   services.caddy.virtualHosts.${domain} = {
     useACMEHost = homelab.baseDomain;
@@ -63,6 +98,9 @@ in {
 
   sops.secrets = {
     "${name}/adminpassword" = {
+      owner = name;
+    };
+    "${name}/pgpassword" = {
       owner = name;
     };
   };
