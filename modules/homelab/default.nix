22eb05df01924e1ae3e5542ad1e5828eceada1d5
EloToJaa <elotoja@protonmail.com>
2025-10-05 12:25:04 +0200

    Add ntfy

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -14,6 +14,7 @@
     ./karakeep
     # ./loki
     # ./nextcloud
+    ./ntfy
     ./paperless
     ./postgres
     # ./prometheus
---
+++ modules/homelab/ntfy/default.nix
@@ -0,0 +1,68 @@
+{
+  variables,
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (variables) homelab;
+  name = "ntfy-sh";
+  domainName = "ntfy";
+  group = variables.homelab.groups.main;
+  port = 3005;
+  dataDir = "${homelab.varDataDir}${name}";
+  domain = "${domainName}.${homelab.baseDomain}";
+in {
+  services.ntfy-sh = {
+    inherit group port dataDir;
+    enable = true;
+    package = pkgs.unstable.ntfy-sh;
+    user = name;
+    environmentFile = config.sops.templates."${name}.env".path;
+    settings = {
+      listen-http = "127.0.0.1:${toString port}";
+      cache-file = "${dataDir}/cache.db";
+      auth-file = "${dataDir}/auth.db";
+      attachment-cache-dir = "${dataDir}/attachments";
+      base-url = domain;
+      auth-default-access = "deny-all";
+      ntfy-behind-proxy = "true";
+      ntfy-enable-login = "true";
+    };
+  };
+  systemd.services.paperless.serviceConfig = {
+    UMask = lib.mkForce homelab.defaultUMask;
+  };
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 750 ${name} ${group} - -"
+  ];
+
+  services.caddy.virtualHosts.${domain} = {
+    useACMEHost = homelab.baseDomain;
+    extraConfig = ''
+      reverse_proxy http://127.0.0.1:${toString port}
+    '';
+  };
+
+  services.restic.backups.appdata-local.paths = [
+    dataDir
+  ];
+
+  users.users.${name} = {
+    isSystemUser = true;
+    description = name;
+    group = lib.mkForce group;
+  };
+
+  sops.secrets = {
+    "${name}/authusers" = {
+      owner = name;
+    };
+  };
+  sops.templates."${name}.env" = {
+    content = ''
+      NTFY_AUTH_USERS=${config.sops.placeholder."${name}/authusers"}
+    '';
+    owner = name;
+  };
+}
