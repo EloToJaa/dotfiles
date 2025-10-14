2acb26e46c5e0d489a007353fbc7a02419d45d35
EloToJaa <elotoja@protonmail.com>
2025-10-15 01:12:16 +0200

    Add main options for homelab module

--- modules/homelab/default.nix
+++ modules/homelab/default.nix
@@ -1,36 +1,102 @@
 {
-  imports = [
-    ./atuin
-    ./backup
-    ./bazarr
-    ./blocky
-    ./caddy
-    ./cleanuparr
-    ./glance
-    # ./grafana
-    ./home-assistant
-    ./immich
-    ./jellyfin
-    ./jellyseerr
-    ./karakeep
-    # ./loki
-    ./nextcloud
-    ./ntfy
-    ./paperless
-    ./postgres
-    # ./prometheus
-    ./prowlarr
-    ./qbittorrent
-    ./radarr
-    ./radicale
-    ./redis
-    # ./rustdesk
-    ./sonarr
-    ./uptime
-    ./vaultwarden
-    ./wireguard
-    ./firewall.nix
-    ./journald.nix
-    ./nat.nix
-  ];
+  config,
+  lib,
+  host,
+  ...
+}: let
+  cfg = config.modules.homelab;
+in {
+  options.modules.homelab = {
+    enable = lib.mkEnableOption "Homelab module";
+    mainDomain = lib.mkOption {
+      type = lib.types.str;
+      default = "elotoja.com";
+    };
+    baseDomain = lib.mkOption {
+      type = lib.types.str;
+      default = "${host}.${cfg.mainDomain}";
+    };
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/opt/";
+    };
+    varDataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/";
+    };
+    logDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/log/";
+    };
+    defaultUMask = lib.mkOption {
+      type = lib.types.str;
+      default = "027";
+    };
+  };
+  options.modules.homelab.groups = {
+    main = lib.mkOption {
+      type = lib.types.str;
+      default = "homelab";
+    };
+    cloud = lib.mkOption {
+      type = lib.types.str;
+      default = "cloud";
+    };
+    media = lib.mkOption {
+      type = lib.types.str;
+      default = "media";
+    };
+    photos = lib.mkOption {
+      type = lib.types.str;
+      default = "photos";
+    };
+    docs = lib.mkOption {
+      type = lib.types.str;
+      default = "documents";
+    };
+    database = lib.mkOption {
+      type = lib.types.str;
+      default = "database";
+    };
+    backups = lib.mkOption {
+      type = lib.types.str;
+      default = "backups";
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    imports = [
+      ./atuin
+      ./backup
+      ./bazarr
+      ./blocky
+      ./caddy
+      ./cleanuparr
+      ./glance
+      # ./grafana
+      ./home-assistant
+      ./immich
+      ./jellyfin
+      ./jellyseerr
+      ./karakeep
+      # ./loki
+      ./nextcloud
+      ./ntfy
+      ./paperless
+      ./postgres
+      # ./prometheus
+      ./prowlarr
+      ./qbittorrent
+      ./radarr
+      ./radicale
+      ./redis
+      # ./rustdesk
+      ./sonarr
+      ./uptime
+      ./vaultwarden
+      ./wireguard
+      ./firewall.nix
+      ./journald.nix
+      ./nat.nix
+    ];
+  };
 }
