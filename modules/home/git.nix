3feb1ede393f89fde7a87c634977d1656828d6e1
EloToJaa <elotoja@protonmail.com>
2025-10-20 22:31:06 +0200

    Fix syntax error in git

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -36,38 +36,39 @@ in {
     enable = lib.mkEnableOption "Enable git";
   };
   config = lib.mkIf cfg.enable {
-    programs.git = {
-      enable = true;
-      package = pkgs.unstable.git;
+    programs = {
+      git = {
+        enable = true;
+        package = pkgs.unstable.git;
 
-      inherit (config.modules.settings.git) userName userEmail;
+        inherit (config.modules.settings.git) userName userEmail;
 
-      extraConfig = {
-        init.defaultBranch = "main";
-        credential.helper = "store";
-        merge.conflictstyle = "diff3";
-        diff.colorMoved = "default";
-        pull.ff = "only";
-        color.ui = true;
-        url = {
-          "https://github.com/EloToJaa/".insteadOf = "etj:";
-          "https://github.com/".insteadOf = "gh:";
+        extraConfig = {
+          init.defaultBranch = "main";
+          credential.helper = "store";
+          merge.conflictstyle = "diff3";
+          diff.colorMoved = "default";
+          pull.ff = "only";
+          color.ui = true;
+          url = {
+            "https://github.com/EloToJaa/".insteadOf = "etj:";
+            "https://github.com/".insteadOf = "gh:";
+          };
+        };
+
+        signing = {
+          signByDefault = false;
         };
       };
-
-      signing = {
-        signByDefault = false;
-      };
-    };
-
-    delta = {
-      enable = true;
-      package = pkgs.unstable.delta;
-      options = {
-        line-numbers = true;
-        side-by-side = true;
-        diff-so-fancy = true;
-        navigate = true;
+      delta = {
+        enable = true;
+        package = pkgs.unstable.delta;
+        options = {
+          line-numbers = true;
+          side-by-side = true;
+          diff-so-fancy = true;
+          navigate = true;
+        };
       };
     };
 
