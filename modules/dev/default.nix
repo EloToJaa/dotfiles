5b0d3afbf9c8175431067964ccc92aa4bfeda16a
EloToJaa <elotoja@protonmail.com>
2025-10-22 23:13:26 +0200

    Add opencode

--- modules/dev/default.nix
+++ modules/dev/default.nix
@@ -4,6 +4,7 @@
     ./scripts # personal scripts
     #./tmux
     ./coding.nix # leetcode cli
+    ./opencode.nix
     ./packages.nix # other packages
     ./variables.nix # environment variables
   ];
