4b13a11d10f17d43744a1072a0d6a3510c0e457e
EloToJaa <elotoja@protonmail.com>
2025-10-24 07:49:47 +0200

    Update laptop config

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -1,76 +1,28 @@
 {
-  pkgs,
   config,
-  lib,
+  inputs,
+  self,
   ...
-}: let
-  inherit (config.settings) username;
-in {
-  imports = [
-    ./hardware-configuration.nix
-    ./config.nix
-  ];
-
-  home-manager.users.${username}.imports = [
-    ./home.nix
-  ];
-
-  networking = {
-    useDHCP = lib.mkForce true;
-  };
-
-  environment.systemPackages = with pkgs; [
-    acpi
-    brightnessctl
-    cpupower-gui
-    powertop
-  ];
-
-  services = {
-    power-profiles-daemon.enable = true;
-
-    upower = {
-      enable = true;
-      percentageLow = 20;
-      percentageCritical = 5;
-      percentageAction = 3;
-      criticalPowerAction = "PowerOff";
+}: {
+  flake.nixosConfigurations = let
+    inherit (inputs.nixpkgs.lib) nixosSystem;
+    inherit (config.settings) username;
+    inherit (self) outputs;
+    specialArgs = {inherit inputs outputs self host;};
+    system = "x86_64-linux";
+    host = "laptop";
+  in {
+    "${host}" = nixosSystem {
+      inherit specialArgs system;
+      modules = [
+        ./hardware-configuration.nix
+        ./config.nix
+        {
+          home-manager.users.${username}.imports = [
+            ./home.nix
+          ];
+        }
+      ];
     };
-
-    tlp.settings = {
-      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-      CPU_BOOST_ON_AC = 1;
-      CPU_BOOST_ON_BAT = 1;
-
-      CPU_HWP_DYN_BOOST_ON_AC = 1;
-      CPU_HWP_DYN_BOOST_ON_BAT = 1;
-
-      PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "performance";
-
-      INTEL_GPU_MIN_FREQ_ON_AC = 500;
-      INTEL_GPU_MIN_FREQ_ON_BAT = 500;
-      # INTEL_GPU_MAX_FREQ_ON_AC=0;
-      # INTEL_GPU_MAX_FREQ_ON_BAT=0;
-      # INTEL_GPU_BOOST_FREQ_ON_AC=0;
-      # INTEL_GPU_BOOST_FREQ_ON_BAT=0;
-
-      # PCIE_ASPM_ON_AC = "default";
-      # PCIE_ASPM_ON_BAT = "powersupersave";
-    };
-  };
-
-  powerManagement.cpuFreqGovernor = "performance";
-
-  boot = {
-    kernelModules = ["acpi_call"];
-    extraModulePackages = with config.boot.kernelPackages;
-      [
-        acpi_call
-        cpupower
-      ]
-      ++ [pkgs.cpupower-gui];
   };
 }
