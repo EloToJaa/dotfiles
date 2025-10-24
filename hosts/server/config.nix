22c8bcfb61cbed80f43af580db307d50602abc94
EloToJaa <elotoja@protonmail.com>
2025-10-24 05:52:28 +0200

    Disable home-assistant

--- hosts/server/config.nix
+++ hosts/server/config.nix
@@ -38,7 +38,7 @@
       cleanuparr.enable = true;
       glance.enable = false;
       grafana.enable = false;
-      home-assistant.enable = true;
+      home-assistant.enable = false;
       immich.enable = true;
       jellyfin.enable = true;
       jellyseerr.enable = true;
