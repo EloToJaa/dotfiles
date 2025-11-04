2ee4c31c5621c2ac01cf08c3fc809164ab5eaeed
EloToJaa <elotoja@protonmail.com>
2025-11-04 19:13:44 +0100

    Update wallpapers

--- homeModules/desktop/waypaper.nix
+++ homeModules/desktop/waypaper.nix
@@ -20,20 +20,20 @@ in {
       */
       ''
         [Settings]
-        language = "en"
-        folder = "~/Pictures/wallpapers/others"
-        monitors = "All"
-        wallpaper = "~/Pictures/wallpapers/others/nixos.png"
-        backend = "swww"
-        fill = "fill"
-        sort = "name"
-        color = "#ffffff"
+        language = en
+        folder = ~/Pictures/wallpapers/others
+        monitors = All
+        wallpaper = ~/Pictures/wallpapers/others/nixos.png
+        backend = swww
+        fill = fill
+        sort = name
+        color = #ffffff
         subfolders = False
         show_hidden = False
         show_gifs_only = False
-        post_command = "pkill .waypaper-wrapp"
+        post_command = pkill .waypaper-wrapp
         number_of_columns = 3
-        swww_transition_type = "any"
+        swww_transition_type = any
         swww_transition_step = 90
         swww_transition_angle = 0
         swww_transition_duration = 2
