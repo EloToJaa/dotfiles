b4e7c8ae9444ba90bfd6ba0a0d9a88271249503d
EloToJaa <elotoja@protonmail.com>
2025-11-04 07:39:31 +0100

    Fix lazygit

--- homeModules/desktop/hyprland/default.nix
+++ homeModules/desktop/hyprland/default.nix
@@ -9,10 +9,7 @@
   imports = [
     ./hyprland.nix
     ./config.nix
-    ./hyprlock.nix
-    ./satty.nix
     ./variables.nix
-    ./wlogout.nix
     inputs.hyprland.homeManagerModules.default
   ];
 }
