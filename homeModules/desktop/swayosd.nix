69cf211a3568198c64a6914782a0c318e4fae84a
EloToJaa <elotoja@protonmail.com>
2025-11-05 07:33:59 +0100

    Update swayosd

--- homeModules/desktop/swayosd.nix
+++ homeModules/desktop/swayosd.nix
@@ -40,45 +40,49 @@ in {
       };
     };
 
-    xdg.configFile."swayosd/style.css".text = ''
-      window {
-          padding: 0px 10px;
-          border-radius: 30px;
-          border: 10px;
-          background: alpha(#111111, 0.99);
-      }
+    xdg.configFile."swayosd/style.css".text =
+      /*
+      css
+      */
+      ''
+        window {
+            padding: 0px 10px;
+            border-radius: 30px;
+            border: 10px;
+            background: alpha(#111111, 0.99);
+        }
 
-      #container {
-          margin: 15px;
-      }
+        #container {
+            margin: 15px;
+        }
 
-      image, label {
-          color: #FBF1C7;
-      }
+        image, label {
+            color: #FBF1C7;
+        }
 
-      progressbar:disabled,
-      image:disabled {
-          opacity: 0.95;
-      }
+        progressbar:disabled,
+        image:disabled {
+            opacity: 0.95;
+        }
 
-      progressbar {
-          min-height: 6px;
-          border-radius: 999px;
-          background: transparent;
-          border: none;
-      }
-      trough {
-          min-height: inherit;
-          border-radius: inherit;
-          border: none;
-          background: alpha(#CCCCCC, 0.1);
-      }
-      progress {
-          min-height: inherit;
-          border-radius: inherit;
-          border: none;
-          background: #FBF1C7;
-      }
-    '';
+        progressbar {
+            min-height: 6px;
+            border-radius: 999px;
+            background: transparent;
+            border: none;
+        }
+        trough {
+            min-height: inherit;
+            border-radius: inherit;
+            border: none;
+            background: alpha(#CCCCCC, 0.1);
+        }
+        progress {
+            min-height: inherit;
+            border-radius: inherit;
+            border: none;
+            background: #FBF1C7;
+        }
+      '';
   };
 }
