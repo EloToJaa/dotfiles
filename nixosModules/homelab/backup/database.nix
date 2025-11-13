aff370df3bc2a2d7f1fff4ffade52d80f769dd66
EloToJaa <elotoja@protonmail.com>
2025-11-13 18:56:17 +0100

    Cleanup db backup

--- nixosModules/homelab/backup/database.nix
+++ nixosModules/homelab/backup/database.nix
@@ -22,13 +22,6 @@ in {
       pgdumpOptions = "--no-owner";
     };
 
-    # systemd.services."postgresqlBackup-db1".serviceConfig = (import ./pg-db-archive.nix) {
-    #   inherit config pkgs;
-    #   dbName = "db1";
-    #   fileEncKey = "/run/secrets/keys/db1";
-    #   fileRclone = "/run/secrets/rclone/database-archive.conf";
-    # };
-
     systemd.services = builtins.listToAttrs (map (dbName: {
         name = "postgresqlBackup-${dbName}";
         value.serviceConfig = mkBackupServiceConfig {
