36b2d90579d8ba16575b2caa8b83de17fe60173c
EloToJaa <elotoja@protonmail.com>
2025-11-14 10:45:02 +0100

    Add nas host

---
+++ hosts/nas/config.nix
@@ -0,0 +1,43 @@
+{
+  networking = {
+    useDHCP = false;
+    interfaces."enp1s0".ipv4.addresses = [
+      {
+        address = "192.168.0.33";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      address = "192.168.0.1";
+      interface = "enp1s0";
+    };
+  };
+
+  powerManagement.cpuFreqGovernor = "performance";
+  imports = [
+    ./../../nixosModules
+  ];
+  modules = {
+    base = {
+      enable = true;
+      bootloader.enable = true;
+      docker.enable = false;
+      index.enable = true;
+      tailscale.enable = true;
+      nfs.enable = false;
+      nh.enable = true;
+      ssh.enable = true;
+    };
+    homelab = {
+      enable = true;
+      backup.enable = false;
+      blocky.enable = false;
+      caddy.enable = true;
+      groups.enable = true;
+      postgres = {
+        enable = true;
+        pgadmin.enable = false;
+      };
+    };
+  };
+}
---
+++ hosts/nas/default.nix
@@ -0,0 +1,28 @@
+{
+  config,
+  inputs,
+  self,
+  ...
+}: {
+  flake.nixosConfigurations = let
+    inherit (inputs.nixpkgs.lib) nixosSystem;
+    inherit (config.settings) username;
+    inherit (self) outputs;
+    specialArgs = {inherit inputs outputs self host;};
+    system = "x86_64-linux";
+    host = "nas";
+  in {
+    "${host}" = nixosSystem {
+      inherit specialArgs system;
+      modules = [
+        ./hardware-configuration.nix
+        ./config.nix
+        {
+          home-manager.users.${username}.imports = [
+            ./home.nix
+          ];
+        }
+      ];
+    };
+  };
+}
