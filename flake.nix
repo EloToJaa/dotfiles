f78bcce2caca23270138c735da733e497eff51af
EloToJaa <elotoja@protonmail.com>
2025-11-16 21:15:50 +0100

    Restore nix-index

--- flake.nix
+++ flake.nix
@@ -41,7 +41,10 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nix-index-database.url = "github:nix-community/nix-index-database";
+    nix-index-database = {
+      url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs-unstable";
+    };
 
     nixvim = {
       url = "github:nix-community/nixvim";
