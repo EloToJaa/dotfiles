915cfdc7a33ea65f3b4e1a638a97bcd8b605c5cb
EloToJaa <elotoja@protonmail.com>
2025-12-07 15:03:54 +0100

    Disable camera module

--- hosts/desktop/config.nix
+++ hosts/desktop/config.nix
@@ -34,7 +34,7 @@
       adb.enable = false;
       audio.enable = true;
       bluetooth.enable = true;
-      camera.enable = true;
+      camera.enable = false;
       gnome.enable = true;
       mullvad.enable = true;
       network.enable = true;
