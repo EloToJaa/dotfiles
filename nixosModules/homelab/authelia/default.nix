10c2c904ecffb84bd4f6c865356fdcb7d2d01eb3
EloToJaa <elotoja@protonmail.com>
2025-12-27 11:35:45 +0100

    Add authelia module

---
+++ nixosModules/homelab/authelia/default.nix
@@ -0,0 +1,74 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}: let
+  inherit (config.modules) homelab;
+  cfg = config.modules.homelab.authelia;
+in {
+  options.modules.homelab.authelia = {
+    enable = lib.mkEnableOption "Enable authelia";
+    name = lib.mkOption {
+      type = lib.types.str;
+      default = "authelia";
+    };
+    domainName = lib.mkOption {
+      type = lib.types.str;
+      default = "auth";
+    };
+    port = lib.mkOption {
+      type = lib.types.port;
+      default = 9091;
+    };
+    group = lib.mkOption {
+      type = lib.types.str;
+      default = homelab.groups.homelab;
+    };
+  };
+  config = lib.mkIf cfg.enable {
+    services.authelia.instances.main = {
+      enable = true;
+      package = pkgs.unstable.authelia;
+      settings = {
+        theme = "dark";
+        default_2fa_method = "mobile_push";
+      };
+    };
+    systemd.tmpfiles.rules = [
+      "d ${homelab.dataDir}${cfg.name} 750 ${cfg.name} ${cfg.group} - -"
+    ];
+
+    services.caddy.virtualHosts."${cfg.domainName}.${homelab.baseDomain}" = {
+      useACMEHost = homelab.baseDomain;
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:${toString cfg.port}
+      '';
+    };
+
+    services.postgresql.ensureUsers = [
+      {
+        inherit (cfg) name;
+        ensureDBOwnership = false;
+      }
+    ];
+    services.postgresql.ensureDatabases = [
+      cfg.name
+    ];
+    services.postgresqlBackup.databases = [
+      cfg.name
+    ];
+
+    sops.secrets = {
+      "${cfg.name}/pgpassword" = {
+        owner = cfg.name;
+      };
+    };
+    sops.templates."${cfg.name}.env" = {
+      content = ''
+        POSTGRES_PASSWORD=${config.sops.placeholder."${cfg.name}/pgpassword"}
+      '';
+      owner = cfg.name;
+    };
+  };
+}
--- nixosModules/homelab/default.nix
+++ nixosModules/homelab/default.nix
@@ -65,6 +65,7 @@ in {
   };
   imports = [
     ./atuin
+    ./authelia
     ./backup
     ./bazarr
     ./blocky
