8fce20cf79e376023ea31c9328b805264a9eb3eb
EloToJaa <elotoja@protonmail.com>
2026-01-02 18:55:02 +0100

    Add authelia config options

--- nixosModules/homelab/authelia/default.nix
+++ nixosModules/homelab/authelia/default.nix
@@ -40,6 +40,10 @@ in {
         sessionSecretFile = config.sops.secrets."${cfg.name}/sessionsecret".path;
         storageEncryptionKeyFile = config.sops.secrets."${cfg.name}/storageencryptionkey".path;
       };
+      environmentVariables = {
+        AUTHELIA_DUO_API_SECRET_KEY_FILE = config.sops.secrets."${cfg.name}/duosecretkey".path;
+        AUTHELIA_STORAGE_POSTGRES_PASSWORD_FILE = config.sops.secrets."${cfg.name}/pgpassword".path;
+      };
       settings = {
         theme = "dark";
         default_2fa_method = "mobile_push";
@@ -56,15 +60,57 @@ in {
           address = "ldap://127.0.0.1:${toString homelab.lldap.ldapPort}";
           implementation = "lldap";
         };
+        duo_api = {
+          hostname = "api-9400ee3a.duosecurity.com";
+          integration_key = "DID9QW95YCI6U92UIS7Q";
+          enable_self_enrollment = true;
+        };
+        storage.postgres = {
+          address = "tcp://127.0.0.1:${toString homelab.postgres.port}";
+          database = cfg.name;
+          username = cfg.name;
+        };
+        notifier = {
+          disable_startup_check = false;
+          filesystem.filename = "${homelab.dataDir}authelia/notification.txt";
+        };
+        session = {
+          name = "authelia_session";
+          expiration = "1h";
+          inactivity = "5m";
+          remember_me = "1M";
+          same_site = "lax";
+          cookies = {
+            inherit (config.services.authelia.instances.main.settings.session) name expiration inactivity same_site;
+            domain = homelab.mainDomain;
+            authelia_url = "https://${cfg.name}.${homelab.mainDomain}";
+            default_redirection_url = "https://${homelab.mainDomain}";
+            remember_me = "1d";
+          };
+          redis = {
+            # host = "unix://${config.services.redis.servers.authelia.unixSocket}";
+            host = config.services.redis.servers.authelia.unixSocket;
+          };
+        };
       };
     };
 
-    services.caddy.virtualHosts."${cfg.domainName}.${homelab.baseDomain}" = {
-      useACMEHost = homelab.baseDomain;
+    services.caddy.virtualHosts = let
       extraConfig = ''
         reverse_proxy http://127.0.0.1:${toString cfg.port}
       '';
+    in {
+      "${cfg.domainName}.${homelab.baseDomain}" = {
+        inherit extraConfig;
+        useACMEHost = homelab.baseDomain;
+      };
+      "${cfg.domainName}.${homelab.mainDomain}" = {
+        inherit extraConfig;
+        useACMEHost = homelab.mainDomain;
+      };
     };
+
+    services.redis.servers.authelia.enable = true;
 
     services.postgresql.ensureUsers = [
       {
@@ -86,15 +132,21 @@ in {
     };
 
     sops.secrets = {
+      "${cfg.name}/jwtsecret" = {
+        owner = cfg.name;
+      };
+      "${cfg.name}/sessionsecret" = {
+        owner = cfg.name;
+      };
+      "${cfg.name}/storageencryptionkey" = {
+        owner = cfg.name;
+      };
       "${cfg.name}/pgpassword" = {
         owner = cfg.name;
       };
-    };
-    sops.templates."${cfg.name}.env" = {
-      content = ''
-        POSTGRES_PASSWORD=${config.sops.placeholder."${cfg.name}/pgpassword"}
-      '';
-      owner = cfg.name;
+      "${cfg.name}/duosecretkey" = {
+        owner = cfg.name;
+      };
     };
   };
 }
