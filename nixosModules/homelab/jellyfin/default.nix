4d1bf2f60b0c66839828737baf811cfdd62332a6
EloToJaa <elotoja@protonmail.com>
2026-01-03 08:18:07 +0100

    Add oic to jellyfin

--- nixosModules/homelab/jellyfin/default.nix
+++ nixosModules/homelab/jellyfin/default.nix
@@ -70,5 +70,34 @@ in {
       description = cfg.name;
       inherit (cfg) group;
     };
+
+    services.authelia.instances.main.settings.identity_providers.oidc.clients = [
+      {
+        client_id = "jellyfin";
+        client_name = "Jellyfin";
+        client_secret = "";
+        public = true;
+        authorization_policy = "two_factor";
+        require_pkce = true;
+        pkce_challenge_method = "S256";
+        redirect_uris = [
+          "https://${cfg.domainName}.${homelab.mainDomain}/sso/OID/redirect/authelia"
+        ];
+        scopes = [
+          "openid"
+          "profile"
+          "groups"
+        ];
+        response_types = [
+          "code"
+        ];
+        grant_types = [
+          "authorization_code"
+        ];
+        access_token_signed_response_alg = "none";
+        userinfo_signed_response_alg = "none";
+        token_endpoint_auth_method = "client_secret_post";
+      }
+    ];
   };
 }
