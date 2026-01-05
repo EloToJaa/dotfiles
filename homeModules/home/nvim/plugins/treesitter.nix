5bc7d67df98533898e1c4b2f8fb7ceb57d206d99
EloToJaa <elotoja@protonmail.com>
2026-01-05 16:40:07 +0100

    Pin treesitter package

--- homeModules/home/nvim/plugins/treesitter.nix
+++ homeModules/home/nvim/plugins/treesitter.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }: let
   cfg = config.modules.home.nvim.plugins.treesitter;
@@ -11,6 +12,7 @@ in {
   config = lib.mkIf cfg.enable {
     programs.nixvim.plugins.treesitter = {
       enable = true;
+      package = pkgs.vimPlugins.nvim-treesitter;
       settings = {
         folding.enable = true;
         highlight.enable = true;
