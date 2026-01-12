43a8173986f519f5370a2a3e4b4d0af73fd4e367
EloToJaa <elotoja@protonmail.com>
2026-01-12 14:03:16 +0100

    Add miro host

--- hosts/default.nix
+++ hosts/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./desktop
     ./laptop
+    ./miro
     # ./nas
     ./server
     ./tester
---
+++ hosts/miro/config.nix
@@ -0,0 +1,46 @@
+{
+  networking = {
+    useDHCP = false;
+    interfaces."enp1s0".ipv4.addresses = [
+      {
+        address = "192.168.0.20";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      address = "192.168.0.1";
+      interface = "enp1s0";
+    };
+  };
+
+  powerManagement.cpuFreqGovernor = "performance";
+  imports = [
+    ./../../nixosModules
+  ];
+  modules = {
+    base = {
+      enable = true;
+      bootloader.enable = true;
+      docker.enable = true;
+      index.enable = true;
+      tailscale.enable = true;
+      nfs.enable = true;
+      nh.enable = true;
+      ssh.enable = true;
+    };
+    homelab = {
+      enable = true;
+      backup.enable = true;
+      bazarr.enable = true;
+      blocky.enable = true;
+      caddy.enable = true;
+      groups.enable = true;
+      home-assistant.enable = true;
+      jellyfin.enable = true;
+      postgres = {
+        enable = true;
+        pgadmin.enable = true;
+      };
+    };
+  };
+}
