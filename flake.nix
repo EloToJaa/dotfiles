51f00a124dfce9f0addeac64c5f34a497498653b
EloToJaa <elotoja@protonmail.com>
2026-01-15 20:07:43 +0100

    fix: flake overlays

--- flake.nix
+++ flake.nix
@@ -17,7 +17,7 @@
       perSystem = {system, ...}: {
         _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = with (import ./overlays {inherit inputs;}).flake.overlays; [modifications unstable-packages];
+          overlays = import ./overlays {inherit inputs;};
           config = {allowUnfree = true;};
         };
       };
