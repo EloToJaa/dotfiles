35d08e42613f8a8a4a93f877db7816e1266c4c62
EloToJaa <elotoja@protonmail.com>
2026-01-15 10:43:34 +0100

    fix: enabled to enable in jellyfin

--- nixosModules/homelab/jellyfin/default.nix
+++ nixosModules/homelab/jellyfin/default.nix
@@ -71,7 +71,7 @@ in {
       inherit (cfg) group;
     };
 
-    services.authelia.instances.main.settings.identity_providers.oidc.clients = lib.mkIf config.services.authelia.instances.main.enabled [
+    services.authelia.instances.main.settings.identity_providers.oidc.clients = lib.mkIf config.services.authelia.instances.main.enable [
       {
         client_id = "jellyfin";
         client_name = "Jellyfin";
