cf7d11b8bfb9b12f82bdffe792c5ddee43b16b5a
EloToJaa <elotoja@protonmail.com>
2026-01-18 13:01:13 +0100

    fix: jellyfin auth config

--- nixosModules/homelab/jellyfin/default.nix
+++ nixosModules/homelab/jellyfin/default.nix
@@ -34,76 +34,78 @@ in {
       default = "${homelab.logDir}${cfg.name}";
     };
   };
-  config = lib.mkIf cfg.enable {
-    services.jellyfin = {
-      enable = true;
-      package = pkgs.unstable.jellyfin;
-      user = cfg.name;
-      inherit (cfg) group dataDir logDir;
-    };
-    systemd.services.jellyfin.serviceConfig.UMask = lib.mkForce homelab.defaultUMask;
-    systemd.tmpfiles.rules = [
-      "d ${cfg.dataDir} 750 ${cfg.name} ${cfg.group} - -"
-      "d ${cfg.logDir} 750 ${cfg.name} ${cfg.group} - -"
-    ];
-    services.restic.backups.appdata-local.paths = [
-      cfg.dataDir
-    ];
-
-    services.caddy.virtualHosts = let
-      extraConfig = ''
-        reverse_proxy http://127.0.0.1:${toString cfg.port}
-      '';
-    in {
-      "${cfg.domainName}.${homelab.baseDomain}" = {
-        inherit extraConfig;
-        useACMEHost = homelab.baseDomain;
+  config =
+    lib.mkIf cfg.enable {
+      services.jellyfin = {
+        enable = true;
+        package = pkgs.unstable.jellyfin;
+        user = cfg.name;
+        inherit (cfg) group dataDir logDir;
       };
-      "${cfg.domainName}.${homelab.mainDomain}" = {
-        inherit extraConfig;
-        useACMEHost = homelab.mainDomain;
+      systemd.services.jellyfin.serviceConfig.UMask = lib.mkForce homelab.defaultUMask;
+      systemd.tmpfiles.rules = [
+        "d ${cfg.dataDir} 750 ${cfg.name} ${cfg.group} - -"
+        "d ${cfg.logDir} 750 ${cfg.name} ${cfg.group} - -"
+      ];
+      services.restic.backups.appdata-local.paths = [
+        cfg.dataDir
+      ];
+
+      services.caddy.virtualHosts = let
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:${toString cfg.port}
+        '';
+      in {
+        "${cfg.domainName}.${homelab.baseDomain}" = {
+          inherit extraConfig;
+          useACMEHost = homelab.baseDomain;
+        };
+        "${cfg.domainName}.${homelab.mainDomain}" = {
+          inherit extraConfig;
+          useACMEHost = homelab.mainDomain;
+        };
+      };
+
+      users.users.${cfg.name} = {
+        isSystemUser = true;
+        description = cfg.name;
+        inherit (cfg) group;
+      };
+    }
+    // lib.mkIf config.services.authelia.instances.main.enable {
+      services.authelia.instances.main.settings.identity_providers.oidc.clients = [
+        {
+          client_id = "jellyfin";
+          client_name = "Jellyfin";
+          client_secret = "{{ secret \"${config.sops.secrets."authelia/secrets/jellyfin".path}\" }}";
+          public = false;
+          authorization_policy = "two_factor";
+          require_pkce = true;
+          pkce_challenge_method = "S256";
+          redirect_uris = [
+            "https://${cfg.domainName}.${homelab.mainDomain}/sso/OID/redirect/authelia"
+          ];
+          scopes = [
+            "openid"
+            "profile"
+            "groups"
+          ];
+          response_types = [
+            "code"
+          ];
+          grant_types = [
+            "authorization_code"
+          ];
+          access_token_signed_response_alg = "none";
+          userinfo_signed_response_alg = "none";
+          token_endpoint_auth_method = "client_secret_post";
+        }
+      ];
+
+      sops.secrets = {
+        "authelia/secrets/jellyfin" = {
+          owner = "authelia";
+        };
       };
     };
-
-    users.users.${cfg.name} = {
-      isSystemUser = true;
-      description = cfg.name;
-      inherit (cfg) group;
-    };
-
-    services.authelia.instances.main.settings.identity_providers.oidc.clients = lib.mkIf config.services.authelia.instances.main.enable [
-      {
-        client_id = "jellyfin";
-        client_name = "Jellyfin";
-        client_secret = "{{ secret \"${config.sops.secrets."authelia/secrets/jellyfin".path}\" }}";
-        public = false;
-        authorization_policy = "two_factor";
-        require_pkce = true;
-        pkce_challenge_method = "S256";
-        redirect_uris = [
-          "https://${cfg.domainName}.${homelab.mainDomain}/sso/OID/redirect/authelia"
-        ];
-        scopes = [
-          "openid"
-          "profile"
-          "groups"
-        ];
-        response_types = [
-          "code"
-        ];
-        grant_types = [
-          "authorization_code"
-        ];
-        access_token_signed_response_alg = "none";
-        userinfo_signed_response_alg = "none";
-        token_endpoint_auth_method = "client_secret_post";
-      }
-    ];
-
-    sops.secrets = {
-      "authelia/secrets/jellyfin" = {
-        owner = "authelia";
-      };
-    };
-  };
 }
