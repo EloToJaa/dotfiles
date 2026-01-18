30137bddc2f03d8cdf7f06b36884ab2252c13954
EloToJaa <elotoja@protonmail.com>
2026-01-18 15:13:21 +0100

    fix: miro ip config

--- hosts/miro/config.nix
+++ hosts/miro/config.nix
@@ -1,19 +1,16 @@
-{lib, ...}: {
-  # networking = {
-  #   useDHCP = false;
-  #   interfaces."enp1s0".ipv4.addresses = [
-  #     {
-  #       address = "192.168.0.22";
-  #       prefixLength = 24;
-  #     }
-  #   ];
-  #   defaultGateway = {
-  #     address = "192.168.0.1";
-  #     interface = "enp1s0";
-  #   };
-  # };
+{
   networking = {
-    useDHCP = lib.mkForce true;
+    useDHCP = false;
+    interfaces."enp1s0".ipv4.addresses = [
+      {
+        address = "192.168.3.22";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      address = "192.168.0.1";
+      interface = "enp1s0";
+    };
   };
 
   settings.isServer = true;
