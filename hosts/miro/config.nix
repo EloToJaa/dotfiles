15b727ba6597d06d1af537b24de79e6add7b903b
EloToJaa <elotoja@protonmail.com>
2026-01-18 10:21:37 +0100

    fix: dynamic ip for miro

--- hosts/miro/config.nix
+++ hosts/miro/config.nix
@@ -1,16 +1,19 @@
-{
+{lib, ...}: {
+  # networking = {
+  #   useDHCP = false;
+  #   interfaces."enp1s0".ipv4.addresses = [
+  #     {
+  #       address = "192.168.0.22";
+  #       prefixLength = 24;
+  #     }
+  #   ];
+  #   defaultGateway = {
+  #     address = "192.168.0.1";
+  #     interface = "enp1s0";
+  #   };
+  # };
   networking = {
-    useDHCP = false;
-    interfaces."enp1s0".ipv4.addresses = [
-      {
-        address = "192.168.0.22";
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = {
-      address = "192.168.0.1";
-      interface = "enp1s0";
-    };
+    useDHCP = lib.mkForce true;
   };
 
   settings.isServer = true;
