6eaf5b019abfba830f89a2de78a6d60f044c67b2
EloToJaa <elotoja@protonmail.com>
2026-03-04 19:19:09 +0100

    fix: wallpaper selector

--- homeModules/desktop/scripts/scripts/wall-change.sh
+++ homeModules/desktop/scripts/scripts/wall-change.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-animations=("fade" "outer" "center" "any")
+animations=("outer" "center" "any" "wipe")
 random_animation=${animations[RANDOM % ${#animations[@]}]}
 
 if [[ "$random_animation" == "wipe" ]]; then
-	swww img --transition-type="wipe" --transition-angle=135 $1 &
+  swww img --transition-type="wipe" --transition-angle=135 "$1" &
 else
-	swww img --transition-type="$random_animation" $1 &
+  swww img --transition-type="$random_animation" "$1" &
 fi
