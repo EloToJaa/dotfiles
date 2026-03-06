7eb9928720c3d49c1e182a19c840f49442fc4aac
EloToJaa <elotoja@protonmail.com>
2026-03-06 07:47:51 +0100

    ssh: enable key generation

--- nixosModules/base/ssh.nix
+++ nixosModules/base/ssh.nix
@@ -16,10 +16,63 @@ in {
     };
   };
   config = lib.mkIf cfg.enable {
+    # SSH Certificate Authority (shared)
+    clan.core.vars.generators.ssh-ca = {
+      share = true;
+      files = {
+        "ca" = {
+          secret = true;
+          deploy = false;
+        };
+        "ca.pub".secret = false;
+      };
+
+      runtimeInputs = [pkgs.openssh];
+      script = ''
+        ssh-keygen -t ed25519 -N "" -f $out/ca
+        mv $out/ca.pub $out/ca.pub
+      '';
+    };
+
+    # Host-specific SSH keys
+    clan.core.vars.generators.ssh-host = {
+      files = {
+        "ssh_host_ed25519_key" = {
+          secret = true;
+          owner = "root";
+          group = "root";
+          mode = "0600";
+        };
+        "ssh_host_ed25519_key.pub".secret = false;
+        "ssh_host_ed25519_key-cert.pub".secret = false;
+      };
+
+      dependencies = ["ssh-ca"];
+      runtimeInputs = [pkgs.openssh];
+      script = ''
+        # Generate host key
+        ssh-keygen -t ed25519 -N "" -f $out/ssh_host_ed25519_key
+
+        # Sign with CA
+        ssh-keygen -s $in/ssh-ca/ca \
+          -I "host:${config.networking.hostName}" \
+          -h \
+          -V -5m:+365d \
+          $out/ssh_host_ed25519_key.pub
+      '';
+    };
+
     programs.ssh.package = pkgs.unstable.openssh;
     services.openssh = {
       enable = true;
       ports = [cfg.port];
+      # Configure SSH to use the generated keys
+      hostKeys = [
+        {
+          path = config.clan.core.vars.generators.ssh-host.files."ssh_host_ed25519_key".path;
+          type = "ed25519";
+        }
+      ];
       settings = {
         PasswordAuthentication = false;
         AllowUsers = null;
@@ -27,7 +80,7 @@ in {
       };
     };
     users.users.${username}.openssh.authorizedKeys.keys = [
-      ssh.keys.user
+      config.clan.core.vars.generators.ssh-host.files."ssh_host_ed25519_key.pub".value
     ];
     networking.firewall.allowedTCPPorts = [
       cfg.port
