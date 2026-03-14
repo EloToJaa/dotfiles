f46b4bf9e1caa83d8a9ee1f8553d93f255623f42
EloToJaa <elotoja@protonmail.com>
2026-03-14 18:40:49 +0100

    rustdesk: add clan backup

--- nixosModules/homelab/rustdesk/default.nix
+++ nixosModules/homelab/rustdesk/default.nix
@@ -5,9 +5,14 @@
   ...
 }: let
   cfg = config.modules.homelab.rustdesk;
+  inherit (config.modules) homelab;
 in {
   options.modules.homelab.rustdesk = {
     enable = lib.mkEnableOption "Enable rustdesk";
+    dataDir = lib.mkOption {
+      type = lib.types.path;
+      default = "${homelab.varDataDir}rustdesk";
+    };
   };
   config = lib.mkIf cfg.enable {
     services.rustdesk-server = {
@@ -19,6 +24,33 @@ in {
         enable = true;
         relayHosts = ["127.0.0.1"];
       };
+    };
+
+    clan.core.state.rustdesk = {
+      folders = [
+        cfg.dataDir
+      ];
+      preBackupScript = ''
+        export PATH=${
+          lib.makeBinPath [
+            config.systemd.package
+          ]
+        }
+
+        systemctl stop rustdesk-signal.service
+        systemctl stop rustdesk-relay.service
+      '';
+
+      postBackupScript = ''
+        export PATH=${
+          lib.makeBinPath [
+            config.systemd.package
+          ]
+        }
+
+        systemctl start rustdesk-signal.service
+        systemctl start rustdesk-relay.service
+      '';
     };
   };
 }
