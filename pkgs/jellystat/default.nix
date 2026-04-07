b2f40acb3f654acf559fecd6a0f921a48c0e54f0
EloToJaa <elotoja@protonmail.com>
2026-04-07 19:38:40 +0200

    jellystat: update

--- pkgs/jellystat/default.nix
+++ pkgs/jellystat/default.nix
@@ -6,13 +6,13 @@
 }:
 buildNpmPackage (finalAttrs: {
   pname = "jellystat";
-  version = "1.1.8";
+  version = "1.1.9";
 
   src = fetchFromGitHub {
     owner = "CyferShepard";
     repo = "Jellystat";
-    tag = "V${finalAttrs.version}";
-    hash = "sha256-3wD9xy+P/edVJnLgImRyDCQ1xgVvkjN07T5JZDoJFY0=";
+    tag = finalAttrs.version;
+    hash = "sha256-M7Gw/TgDB+vA5wtYf5vLxLZ5r9D8B9pVEBE0FiRGlKs=";
   };
 
   npmDepsHash = "sha256-Y40ZnpHjEbYOjDrgwjLxCTyGWHGH6Zw8JADUiJc4hl4=";
@@ -46,7 +46,7 @@ buildNpmPackage (finalAttrs: {
   meta = {
     description = "Free and open source Statistics App for Jellyfin";
     homepage = "https://github.com/CyferShepard/Jellystat";
-    changelog = "https://github.com/CyferShepard/Jellystat/releases/tag/V${finalAttrs.version}";
+    changelog = "https://github.com/CyferShepard/Jellystat/releases/tag/${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = [lib.maintainers.kashw2];
   };
