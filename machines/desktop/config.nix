e233c26e808cde27206a1a16f430dee0880eb529
EloToJaa <elotoja@protonmail.com>
2026-05-18 13:55:40 +0200

    feat(network): set desktop & server to dhcp

--- machines/desktop/config.nix
+++ machines/desktop/config.nix
@@ -1,17 +1,6 @@
-{
+{lib, ...}: {
   networking = {
-    # useDHCP = lib.mkForce true;
-    useDHCP = false;
-    interfaces."eno1".ipv4.addresses = [
-      {
-        address = "192.168.0.20";
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = {
-      address = "192.168.0.1";
-      interface = "eno1";
-    };
+    useDHCP = lib.mkForce true;
   };
 
   powerManagement.cpuFreqGovernor = "performance";
