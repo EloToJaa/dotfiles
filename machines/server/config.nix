cbc66475cac3d23134390e65d0bd74f8ae8619e9
EloToJaa <elotoja@protonmail.com>
2026-05-19 06:53:46 +0200

    fix(network): set server to static

--- machines/server/config.nix
+++ machines/server/config.nix
@@ -1,6 +1,17 @@
-{lib, ...}: {
+{
   networking = {
-    useDHCP = lib.mkForce true;
+    # useDHCP = lib.mkForce true;
+    useDHCP = false;
+    interfaces."enp1s0".ipv4.addresses = [
+      {
+        address = "192.168.0.32";
+        prefixLength = 24;
+      }
+    ];
+    defaultGateway = {
+      address = "192.168.0.1";
+      interface = "enp1s0";
+    };
   };
 
   settings.isServer = true;
