9da92b93726b87eb88a8d65749cae2f821127559
EloToJaa <elotoja@protonmail.com>
2026-05-21 20:20:47 +0200

    feat(pi): create extensions

--- homeModules/ai/pi.nix
+++ homeModules/ai/pi.nix
@@ -15,9 +15,20 @@ in {
     home.packages = [
       pkgs.llm-agents.pi
     ];
-    home.file.".pi/agent/extensions" = {
-      recursive = true;
-      source = extensions;
+    home.file = {
+      ".pi/agent" = {
+        recursive = true;
+        source = ./pi;
+      };
+      ".pi/agent/extensions/air".source = "${extensions}/air";
+      ".pi/agent/extensions/direnv".source = "${extensions}/direnv";
+      ".pi/agent/extensions/fetch".source = "${extensions}/fetch";
+      ".pi/agent/extensions/notify".source = "${extensions}/notify";
+      ".pi/agent/extensions/permission-gate".source = "${extensions}/permission-gate";
+      ".pi/agent/extensions/questionnaire".source = "${extensions}/questionnaire";
+      ".pi/agent/extensions/slow-mode".source = "${extensions}/slow-mode";
+      ".pi/agent/extensions/stash".source = "${extensions}/stash";
+      ".pi/agent/extensions/statusline".source = "${extensions}/statusline";
     };
   };
 }
