ebce665ea265e1fdbe5ff48ac7471e74d58b9c2b
EloToJaa <elotoja@protonmail.com>
2026-06-01 21:41:03 +0200

    fix(inotify): update max_user_watches

--- nixosModules/base/bootloader.nix
+++ nixosModules/base/bootloader.nix
@@ -21,7 +21,7 @@ in {
         efi.canTouchEfiVariables = true;
       };
       kernel.sysctl = {
-        "fs.inotify.max_user_watches" = 524288;
+        "fs.inotify.max_user_watches" = 1048576;
         "fs.inotify.max_user_instances" = 1024;
         "fs.inotify.max_queued_events" = 32768;
       };
