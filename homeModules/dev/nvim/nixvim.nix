67850526aeba1202b73675705ff059e93d6b6423
EloToJaa <elotoja@protonmail.com>
2026-07-03 09:34:19 +0200

    fix(nixvim): disable nixpkgs release check

--- homeModules/dev/nvim/nixvim.nix
+++ homeModules/dev/nvim/nixvim.nix
@@ -14,6 +14,7 @@ in {
       defaultEditor = true;
 
       nixpkgs.useGlobalPackages = true;
+      version.enableNixpkgsReleaseCheck = false;
 
       package = pkgs.unstable.neovim-unwrapped;
     };
