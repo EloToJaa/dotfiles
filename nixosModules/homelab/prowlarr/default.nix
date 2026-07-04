ac680ba1eb32d9f4add426a96d3d30f0a19e4d73
EloToJaa <elotoja@protonmail.com>
2026-07-04 22:49:33 +0200

    fix(prowlarr): fix arr connections

--- nixosModules/homelab/prowlarr/default.nix
+++ nixosModules/homelab/prowlarr/default.nix
@@ -60,42 +60,108 @@ in {
         ];
       }
       // (lib.mkIf config.services.wireguard-netns.enable {
-        services.${cfg.name} = {
-          bindsTo = ["netns@${ns}.service"];
-          requires = [
-            "network-online.target"
-            "${ns}.service"
-          ];
-          serviceConfig.NetworkNamespacePath = ["/var/run/netns/${ns}"];
-        };
-        sockets."${cfg.name}-proxy" = {
-          enable = true;
-          description = "Socket for Proxy to ${cfg.name}";
-          listenStreams = [(toString cfg.port)];
-          wantedBy = ["sockets.target"];
-        };
-        services."${cfg.name}-proxy" = {
-          enable = true;
-          description = "Proxy to ${cfg.name} in Network Namespace";
-          requires = [
-            "${cfg.name}.service"
-            "${cfg.name}-proxy.socket"
-          ];
-          after = [
-            "${cfg.name}.service"
-            "${cfg.name}-proxy.socket"
-          ];
-          unitConfig = {
-            JoinsNamespaceOf = "${cfg.name}.service";
+        services =
+          {
+            ${cfg.name} = {
+              bindsTo = ["netns@${ns}.service"];
+              requires = [
+                "network-online.target"
+                "${ns}.service"
+              ];
+              serviceConfig.NetworkNamespacePath = ["/var/run/netns/${ns}"];
+            };
+            "${cfg.name}-proxy" = {
+              enable = true;
+              description = "Proxy to ${cfg.name} in Network Namespace";
+              requires = [
+                "${cfg.name}.service"
+                "${cfg.name}-proxy.socket"
+              ];
+              after = [
+                "${cfg.name}.service"
+                "${cfg.name}-proxy.socket"
+              ];
+              unitConfig = {
+                JoinsNamespaceOf = "${cfg.name}.service";
+              };
+              serviceConfig = {
+                User = cfg.name;
+                Group = cfg.group;
+                ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:${toString cfg.port}";
+                PrivateNetwork = "yes";
+              };
+            };
+          }
+          // lib.optionalAttrs homelab.radarr.enable {
+            "${cfg.name}-radarr-proxy" = {
+              enable = true;
+              description = "Proxy from ${cfg.name} namespace to radarr";
+              requires = ["${cfg.name}-radarr-proxy.socket"];
+              after = ["${cfg.name}-radarr-proxy.socket"];
+              serviceConfig.ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:${toString homelab.radarr.port}";
+            };
+          }
+          // lib.optionalAttrs homelab.sonarr.enable {
+            "${cfg.name}-sonarr-proxy" = {
+              enable = true;
+              description = "Proxy from ${cfg.name} namespace to sonarr";
+              requires = ["${cfg.name}-sonarr-proxy.socket"];
+              after = ["${cfg.name}-sonarr-proxy.socket"];
+              serviceConfig.ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:${toString homelab.sonarr.port}";
+            };
+          }
+          // lib.optionalAttrs homelab.lidarr.enable {
+            "${cfg.name}-lidarr-proxy" = {
+              enable = true;
+              description = "Proxy from ${cfg.name} namespace to lidarr";
+              requires = ["${cfg.name}-lidarr-proxy.socket"];
+              after = ["${cfg.name}-lidarr-proxy.socket"];
+              serviceConfig.ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:${toString homelab.lidarr.port}";
+            };
           };
-          serviceConfig = {
-            User = cfg.name;
-            Group = cfg.group;
-            ExecStart = "${config.systemd.package}/lib/systemd/systemd-socket-proxyd --exit-idle-time=5min 127.0.0.1:${toString cfg.port}";
-            PrivateNetwork = "yes";
+        sockets =
+          {
+            "${cfg.name}-proxy" = {
+              enable = true;
+              description = "Socket for Proxy to ${cfg.name}";
+              listenStreams = [(toString cfg.port)];
+              wantedBy = ["sockets.target"];
+            };
+          }
+          // lib.optionalAttrs homelab.radarr.enable {
+            "${cfg.name}-radarr-proxy" = {
+              enable = true;
+              description = "Socket for ${cfg.name} namespace access to radarr";
+              listenStreams = ["${config.services.wireguard-netns.hostAddress}:${toString homelab.radarr.port}"];
+              wantedBy = ["sockets.target"];
+              socketConfig.FreeBind = true;
+            };
+          }
+          // lib.optionalAttrs homelab.sonarr.enable {
+            "${cfg.name}-sonarr-proxy" = {
+              enable = true;
+              description = "Socket for ${cfg.name} namespace access to sonarr";
+              listenStreams = ["${config.services.wireguard-netns.hostAddress}:${toString homelab.sonarr.port}"];
+              wantedBy = ["sockets.target"];
+              socketConfig.FreeBind = true;
+            };
+          }
+          // lib.optionalAttrs homelab.lidarr.enable {
+            "${cfg.name}-lidarr-proxy" = {
+              enable = true;
+              description = "Socket for ${cfg.name} namespace access to lidarr";
+              listenStreams = ["${config.services.wireguard-netns.hostAddress}:${toString homelab.lidarr.port}"];
+              wantedBy = ["sockets.target"];
+              socketConfig.FreeBind = true;
+            };
           };
-        };
       });
+
+    networking.firewall.interfaces.wg-host.allowedTCPPorts = lib.mkIf config.services.wireguard-netns.enable (
+      lib.optionals homelab.radarr.enable [homelab.radarr.port]
+      ++ lib.optionals homelab.sonarr.enable [homelab.sonarr.port]
+      ++ lib.optionals homelab.lidarr.enable [homelab.lidarr.port]
+    );
 
     services.nginx.virtualHosts."${cfg.domainName}.${homelab.baseDomain}" = {
       forceSSL = true;
