5406a942f37b8952f62252ddfffde33855ae9ec1
EloToJaa <elotoja@protonmail.com>
2026-09-22 07:28:13 +0200

    refactor(niri): use package paths for startup commands

--- homeModules/desktop/niri/spawn.nix
+++ homeModules/desktop/niri/spawn.nix
@@ -1,24 +1,27 @@
 {
   config,
   lib,
+  pkgs,
+  inputs,
   settings,
   ...
 }: let
   cfg = config.modules.desktop.niri;
   inherit (settings) discord;
+  niri-session-manager = inputs.niri-session-manager.packages.${pkgs.stdenv.hostPlatform.system}.default;
 in {
   config = lib.mkIf cfg.enable {
     wayland.windowManager.niri.settings._children = map (args: {spawn-at-startup._args = args;}) [
       # System tray apps
       # ["poweralertd"]
       # ["wl-clip-persist" "--clipboard" "both"]
-      ["udiskie" "--automount" "--notify" "--smart-tray"]
+      [(lib.getExe' pkgs.udiskie "udiskie") "--automount" "--notify" "--smart-tray"]
       [discord]
       # ["opencloud"]
-      ["valent" "--gapplication-service"]
-      ["ntfy" "subscribe" "--from-config"]
-      ["oniri" "--edges-maximizing"]
-      ["niri-session-manager"]
+      [(lib.getExe pkgs.unstable.valent) "--gapplication-service"]
+      [(lib.getExe pkgs.ntfy-sh) "subscribe" "--from-config"]
+      [(lib.getExe pkgs.oniri) "--edges-maximizing"]
+      [(lib.getExe niri-session-manager)]
     ];
   };
 }
